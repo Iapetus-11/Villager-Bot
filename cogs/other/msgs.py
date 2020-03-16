2fef8a1114a9158965a2f541697c0c3706870230
Iapetus-11 <olimdahacker@gmail.com>
2020-03-16 17:09:10 -0400

    minor tweaks

--- cogs/other/msgs.py
+++ cogs/other/msgs.py
@@ -8,16 +8,22 @@ class Msgs(commands.Cog):
         self.db = self.bot.get_cog("Database")
         self.g = self.bot.get_cog("Global")
         
+    async def send(self, ctx, msg):
+        try:
+            await ctx.send(msg)
+        except Exception:
+            pass
+        
     @commands.Cog.listener()
     async def on_message(self, message):
         self.g.msg_count += 1
         await self.db.incrementVaultMax(message.author.id)
-        if "emerald" in message.clean_content.lower() or "villager bot" in message.clean_content.lower():
-            if not message.guild == None:
-                if await self.db.getDoReplies(message.guild.id):
-                    await message.channel.send(choice(["hrmm", "hmm", "hrmmm", "hrghhmmm", "hrhhmmmmmmmmm", "hrmmmmmm", "hrmmmmmmmmmm", "hrmmmmm"]))
+        
+        if "emerald" in message.clean_content.lower() or "villager bot" in message.clean_content.lower():            
+            if not message.guild is None and await self.db.getDoReplies(message.guild.id):
+                await self.send(message.channel, choice(["hrmm", "hmm", "hrmmm", "hrghhmmm", "hrhhmmmmmmmmm", "hrmmmmmm", "hrmmmmmmmmmm", "hrmmmmm"]))
             else:
-                await message.channel.send(choice(["hrmm", "hmm", "hrmmm", "hrghhmmm", "hrhhmmmmmmmmm", "hrmmmmmm", "hrmmmmmmmmmm", "hrmmmmm"]))
+                await self.send(message.channel, choice(["hrmm", "hmm", "hrmmm", "hrghhmmm", "hrhhmmmmmmmmm", "hrmmmmmm", "hrmmmmmmmmmm", "hrmmmmm"]))
                 
 def setup(bot):
     bot.add_cog(Msgs(bot))
