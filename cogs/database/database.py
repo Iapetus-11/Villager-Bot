b0c205071003b4629809ca6089c7aa325ec56d1e
Iapetus-11 <38477514+Iapetus-11@users.noreply.github.com>
2020-03-26 22:12:05 -0400

    Clean up some of the nasty code

--- cogs/database/database.py
+++ cogs/database/database.py
@@ -19,10 +19,10 @@ class Database(commands.Cog):
 
     async def getdbv(self, table, uid, two, sett): # table(table in database), uid(context user id), two(second column with data, not uid), sett(default value that it is set to if other entry isn't there)
         cur = self.db.cursor()
-        cur.execute("SELECT "+str(two)+" FROM "+str(table)+" WHERE "+str(table)+".id='"+str(uid)+"'")
+        cur.execute(f"SELECT {two} FROM {table} WHERE {table}.id='{uid}'")
         val = cur.fetchone()
         if val is None:
-            cur.execute("INSERT INTO "+str(table)+" VALUES ('"+str(uid)+"', '"+str(sett)+"')")
+            cur.execute(f"INSERT INTO {table} VALUES ('{uid}', '{sett}')")
             self.db.commit()
             val = (sett,)
         return str(val[0])
@@ -30,17 +30,17 @@ class Database(commands.Cog):
     async def setdbv(self, table, uid, two, sett):
         await self.getdbv(table, uid, two, sett)
         cur = self.db.cursor()
-        cur.execute("UPDATE "+str(table)+" SET "+str(two)+"='"+str(sett)+"' WHERE id='"+str(uid)+"'")
+        cur.execute(f"UPDATE {table} SET {two}='{sett}' WHERE id='{uid}'")
         self.db.commit()
 
     async def getdbv3(self, table, uid, two, three, sett, settt): # table, user id, second column with data, third column with data, 2nd column default val, 3rd column default val
         cur = self.db.cursor()
-        cur.execute("SELECT "+str(two)+" FROM "+str(table)+" WHERE "+str(table)+".id='"+str(uid)+"'")
+        cur.execute(f"SELECT {two} FROM {table} WHERE {table}.id='{uid}'")
         val2 = cur.fetchone()
-        cur.execute("SELECT "+str(three)+" FROM "+str(table)+" WHERE "+str(table)+".id='"+str(uid)+"'")
+        cur.execute(f"SELECT {three} FROM {table} WHERE {table}.id='{uid}'")
         val3 = cur.fetchone()
         if val2 is None or val3 is None:
-            cur.execute("INSERT INTO "+str(table)+" VALUES ('"+str(uid)+"', '"+str(sett)+"', '"+str(settt)+"')")
+            cur.execute(f"INSERT INTO {table} VALUES ('{uid}', '{sett}', '{settt}')")
             self.db.commit()
             vals = (sett, settt,)
         else:
@@ -50,18 +50,16 @@ class Database(commands.Cog):
     async def setdbv3(self, table, uid, two, three, sett, settt):
         await self.getdbv3(table, uid, two, three, sett, settt)
         cur = self.db.cursor()
-        cur.execute("UPDATE "+str(table)+" SET "+str(two)+"='"+str(sett)+"' WHERE id='"+str(uid)+"'")
-        cur.execute("UPDATE "+str(table)+" SET "+str(three)+"='"+str(settt)+"' WHERE id='"+str(uid)+"'")
+        cur.execute(f"UPDATE {table} SET {two}='{sett}' WHERE id='{uid}'")
+        cur.execute(f"UPDATE {table} SET {three}='{settt}' WHERE id='{uid}'")
         self.db.commit()
 
-    async def incrementMax(self, ctx):
+    async def incrementVaultMax(self, ctx):
         user = ctx.author
         vault = await self.getdbv3("vault", user.id, "amount", "max", 0, 0)
         if vault[1] < 2000:
             if choice([True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False,
-                       False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False,
-                       False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False,
-                       False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]):
+                       False, False, False, False, False, False, False, False]):
                 await self.setdbv3("vault", user.id, "amount", "max", vault[0], int(vault[1])+1)
 
     async def getBal(self, uid): # Gets emeralds
@@ -95,18 +93,12 @@ class Database(commands.Cog):
     async def setVault(self, uid, amount, maxx):
         await self.setdbv3("vault", uid, "amount", "max", amount, maxx)
 
-    async def incrementVaultMax(self, uid):
-        vault = await self.getVault(uid)
-        if vault[1] < 2000:
-            if choice([True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]):
-                await self.setVault(uid, vault[0], vault[1]+1)
-
     async def botBan(self, uid):
         cur = self.db.cursor()
-        cur.execute("SELECT id FROM bans WHERE bans.id='"+str(uid)+"'")
+        cur.execute(f"SELECT id FROM bans WHERE bans.id='{uid}'")
         entry = cur.fetchone()
         if entry is None:
-            cur.execute("INSERT INTO bans VALUES ('"+str(uid)+"')")
+            cur.execute(f"INSERT INTO bans VALUES ('{uid}')")
             self.db.commit()
             return "Successfully banned {0}."
         else:
@@ -114,12 +106,12 @@ class Database(commands.Cog):
 
     async def botUnban(self, uid):
         cur = self.db.cursor()
-        cur.execute("SELECT id FROM bans WHERE bans.id='"+str(uid)+"'")
+        cur.execute(f"SELECT id FROM bans WHERE bans.id='{uid}'")
         entry = cur.fetchone()
         if entry is None:
             return "{0} was not banned."
         else:
-            cur.execute("DELETE FROM bans WHERE bans.id='"+str(uid)+"'")
+            cur.execute(f"DELETE FROM bans WHERE bans.id='{uid}'")
             self.db.commit()
             return "{0} was successfully unbanned."
         
@@ -131,10 +123,10 @@ class Database(commands.Cog):
 
     async def getPrefix(self, gid):
         cur = self.db.cursor()
-        cur.execute("SELECT prefix FROM prefixes WHERE prefixes.gid='"+str(gid)+"'")
+        cur.execute(f"SELECT prefix FROM prefixes WHERE prefixes.gid='{gid}'")
         prefix = cur.fetchone()
         if prefix is None:
-            cur.execute("INSERT INTO prefixes VALUES ('"+str(gid)+"', '!!')")
+            cur.execute(f"INSERT INTO prefixes VALUES ('{gid}', '!!')")
             self.db.commit()
             return "!!"
         return prefix[0]
@@ -142,15 +134,15 @@ class Database(commands.Cog):
     async def setPrefix(self, gid, prefix):
         await self.getPrefix(gid)
         cur = self.db.cursor()
-        cur.execute("UPDATE prefixes SET prefix='{0}' WHERE gid='{1}'".format(prefix, gid))
+        cur.execute(f"UPDATE prefixes SET prefix='{prefix}' WHERE gid='{gid}'")
         self.db.commit()
 
     async def getDoReplies(self, gid):
         cur = self.db.cursor()
-        cur.execute("SELECT reply FROM doreplies WHERE doreplies.gid='"+str(gid)+"'")
+        cur.execute(f"SELECT reply FROM doreplies WHERE doreplies.gid='{gid}'")
         dothatshit = cur.fetchone()
         if dothatshit is None:
-            cur.execute("INSERT INTO doreplies VALUES ('"+str(gid)+"', true)")
+            cur.execute(f"INSERT INTO doreplies VALUES ('{gid}', true)")
             self.db.commit()
             return True
         return dothatshit[0]
@@ -158,12 +150,12 @@ class Database(commands.Cog):
     async def setDoReplies(self, gid, doit):
         await self.getDoReplies(gid)
         cur = self.db.cursor()
-        cur.execute("UPDATE doreplies SET reply={0} WHERE gid='{1}'".format(doit, gid))
+        cur.execute(f"UPDATE doreplies SET reply={doit} WHERE gid='{gid}'")
         self.db.commit()
 
     async def dropDoReplies(self, gid):
         cur = self.db.cursor()
-        cur.execute("DELETE FROM doreplies WHERE doreplies.gid='{0}'".format(gid))
+        cur.execute(f"DELETE FROM doreplies WHERE doreplies.gid='{gid}'")
         self.db.commit()
 
     async def getItems(self, uid):
