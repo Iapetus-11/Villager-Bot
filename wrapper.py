10c33bf6cf4dc4027cadfc1ef4a366c762293a1d
Iapetus-11 <olimdahacker@gmail.com>
2020-04-11 18:35:33 -0400

    bug fixes?

--- wrapper.py
+++ wrapper.py
@@ -1,7 +1,7 @@
 import subprocess
 
 while True:
-    p = subprocess.Popen("villager-bot.sh", stdout=subprocess.PIPE, shell=True)
+    p = subprocess.Popen("python3 bot.py", stdout=subprocess.PIPE, shell=True)
     for line in p.stdout:
         print(line)
         if "blocked" in line.lower():
