b60cc6f223e012ff31c8a91c7d6311674d4a1348
Iapetus-11 <olimdahacker@gmail.com>
2020-04-12 09:57:44 -0400

    minor improvements

--- villager-bot.py
+++ villager-bot.py
@@ -2,9 +2,12 @@ import subprocess
 
 while True:
     p = subprocess.Popen("python3 bot.py", stderr=subprocess.PIPE, shell=True)
-    for line in p.stderr:
-        print(line.decode("utf-8").rstrip("\n"))
-        if "heartbeat blocked" in line.decode("utf-8").lower():
-            break
+    stop = False
+    while not stop:
+        for line in p.stderr:
+            print(line.decode("utf-8").rstrip("\n"))
+            if "heartbeat blocked" in line.decode("utf-8").lower():
+                stop = True
+                break
     print("== BOT EXITED ==")
-    p.terminate()
+    p.kill()
