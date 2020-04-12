5bdbb3bff5682ccd5554cf628abcf94485c6cf32
Iapetus-11 <olimdahacker@gmail.com>
2020-04-12 11:14:36 -0400

    bug fixes

--- cogs/commands/useful.py
+++ cogs/commands/useful.py
@@ -98,7 +98,7 @@ f'**{ctx.prefix}battle** ***user*** *allows you to battle your friends!*\n',
             f'**{ctx.prefix}google** ***query*** *bot will search on google for your query*\n'
             f'**{ctx.prefix}youtube** ***query*** *bot will search on youtube for your query*\n'
             f'**{ctx.prefix}reddit** ***query*** *bot will search on reddit for your query*\n'
-            f'**{Ctx.prefix}news** *shows what\'s new with the bot*\n',
+            f'**{ctx.prefix}news** *shows what\'s new with the bot*\n',
             inline=True)
 
         help_embed.set_footer(text=choice(self.tips))
