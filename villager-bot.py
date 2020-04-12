90a683d438045a76d07a6635dcd7756acebb4d7f
Iapetus-11 <olimdahacker@gmail.com>
2020-04-11 22:40:21 -0400

    bug fixes

--- villager-bot.py
+++ villager-bot.py
@@ -2,9 +2,12 @@ import subprocess
 
 while True:
     p = subprocess.Popen("python3 bot.py", stdout=subprocess.PIPE, shell=True)
-    for line in p.stdout:
-        print(line)
-        if "heartbeat" in line.lower():
-            break
+    stop = False
+    while not stop:
+        for line in p.stdout:
+            print(line)
+            if "heartbeat" in line.lower():
+                stop = True
+                break
     print("== BOT EXITED ==")
     p.terminate()
