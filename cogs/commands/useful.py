b0dafa097114a55c2854ad04b09445ae8000e1ae
Iapetus-11 <olimdahacker@gmail.com>
2020-04-23 20:39:09 -0400

    update news

--- cogs/commands/useful.py
+++ cogs/commands/useful.py
@@ -176,7 +176,8 @@ f'**{ctx.prefix}battle** ***user*** *allows you to battle your friends!*\n',
         emb.set_author(name="What's new with Villager Bot?", url=discord.Embed.Empty, icon_url="http://olimone.ddns.net/images/villagerbotsplash1.png")
         emb.add_field(name="Bot Updates", value="- Search commands got an overhaul\n"
                                                 "- New ``!!image`` command which searches google images\n"
-                                                "- New ``!!fish`` command and new enchantment book: Luck Of The Sea!", inline=False)
+                                                "- New ``!!fish`` command and new enchantment book: Luck Of The Sea!"
+                                                "- New fish bois", inline=False)
         emb.set_footer(text=choice(self.tips))
         await ctx.send(embed=emb)
 
