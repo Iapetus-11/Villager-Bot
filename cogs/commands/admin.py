1c9d82a4ffec4b666afcf53963c265b63f0766db
Iapetus-11 <olimdahacker@gmail.com>
2020-04-24 15:24:15 -0400

    graphical improvements

--- cogs/commands/admin.py
+++ cogs/commands/admin.py
@@ -91,19 +91,19 @@ class AdminCmds(commands.Cog):
             return
         reason = (reason[:125] + "...") if len(reason) > 128 else reason
         await self.db.add_warn(user.id, ctx.author.id, ctx.guild.id, reason)
-        await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"{user} was warned by {ctx.author} for reason: *{reason}*"))
+        await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"**{user}** was warned by **{ctx.author}** for reason: *{reason}*"))
 
     @commands.command(name="warns", aliases=["getwarns", "getuserwarns", "warnings"])
     @commands.guild_only()
     @commands.check_any(commands.has_permissions(administrator=True), commands.has_permissions(kick_members=True), commands.has_permissions(ban_members=True))
     async def get_user_warns(self, ctx, user: discord.User):
         user_warns = await self.db.get_warns(user.id, ctx.guild.id)
-        embed = discord.Embed(color=discord.Color.green(), title=f"{user}'s Warnings ({len(user_warns)} total):")
+        embed = discord.Embed(color=discord.Color.green(), title=f"**{user}**'s Warnings ({len(user_warns)} total):")
         if len(user_warns) == 0:
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), title=f"{user}'s Warnings ({len(user_warns)} total):", description=f"{user} has no warnings in this server."))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(), title=f"**{user}**'s Warnings ({len(user_warns)} total):", description=f"**{user}** has no warnings in this server."))
             return
         for warning in user_warns:
-            embed.add_field(name=f"**Warning by {self.bot.get_user(warning[1])}**:", value=warning[3], inline=False)
+            embed.add_field(name=f"Warning by **{self.bot.get_user(warning[1])}**:", value=warning[3], inline=False)
         await ctx.send(embed=embed)
 
     @commands.command(name="clearwarns", aliases=["deletewarns", "removewarns"])
@@ -112,10 +112,10 @@ class AdminCmds(commands.Cog):
     async def clear_user_warns(self, ctx, user: discord.User):
         user_warns = await self.db.get_warns(user.id, ctx.guild.id)
         if len(user_warns) == 0:
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"{user} has no warns."))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"**{user}** has no warns."))
         else:
             await self.db.clear_warns(user.id, ctx.guild.id)
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"Cleared all of {user}'s warns."))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"Cleared all of **{user}**'s warns."))
 
 
 def setup(bot):
