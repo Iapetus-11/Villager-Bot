7a8adc865b590e41ffccdddec3f13867612f699f
Iapetus-11 <olimdahacker@gmail.com>
2020-04-24 13:03:19 -0400

    stats command stuff

--- cogs/commands/useful.py
+++ cogs/commands/useful.py
@@ -289,6 +289,7 @@ User Count: ``{len(self.bot.users)}``
 Session Message Count: ``{self.g.msg_count}``
 Session Command Count: ``{self.g.cmd_count} ({round((self.g.cmd_count/self.g.msg_count)*100, 2)}% of all msgs)``
 Commands/Sec: ``{self.g.cmd_vect}``
+Session Vote Count: ``{self.g.vote_count}``
 Top.gg Votes/Hour: ``{self.g.vote_vect}``
 Shard Count: ``{self.bot.shard_count}``
 Latency: ``{round(self.bot.latency*1000, 2)} ms``
