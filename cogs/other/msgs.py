7acef7a0ebfa48e9c24bb829827b603060e42609
Iapetus-11 <olimdahacker@gmail.com>
2020-04-25 17:20:45 -0400

    modify bot's response when it's mentioned

--- cogs/other/msgs.py
+++ cogs/other/msgs.py
@@ -18,18 +18,11 @@ class Msgs(commands.Cog):
                 prefix = await self.db.get_prefix(message.guild.id)
             else:
                 prefix = "!!"
-            help_embed = discord.Embed(color=discord.Color.green())
+            help_embed = discord.Embed(color=discord.Color.green(),
+                                       description=f"The prefix for this server is ``{prefix}``\nFor help, either join the [support server](https://discord.gg/39DwwUV) or use the ``{prefix}help`` command.")
             help_embed.set_author(
-                name="Villager Bot Commands",
+                name="Villager Bot",
                 icon_url="http://olimone.ddns.net/images/villagerbotsplash1.png")
-            help_embed.add_field(name="Minecraft", value=f"``{prefix}help mc``", inline=True)
-            help_embed.add_field(name="Fun", value=f"``{prefix}help fun``", inline=True)
-            help_embed.add_field(name="\uFEFF", value=f"\uFEFF", inline=True)
-            help_embed.add_field(name="Useful", value=f"``{prefix}help useful``", inline=True)
-            help_embed.add_field(name="Admin", value=f"``{prefix}help admin``", inline=True)
-            help_embed.add_field(name="\uFEFF", value=f"\uFEFF", inline=True)
-            help_embed.add_field(name="\uFEFF", value="""Need more help? Check out the Villager Bot [Support Server](https://discord.gg/39DwwUV)
-            Enjoying the bot? Vote for us on [top.gg](https://top.gg/bot/639498607632056321/vote)""", inline=False)
             help_embed.set_footer(text=choice(self.bot.get_cog("Useful").tips))
             try:
                 await message.channel.send(embed=help_embed)
