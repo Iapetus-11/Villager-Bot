c811914fa6dafa6acd50b2e7442eb86564aa2aba
Iapetus-11 <olimdahacker@gmail.com>
2020-04-26 14:15:13 -0400

    change stop() to close()

--- cogs/commands/mc.py
+++ cogs/commands/mc.py
@@ -30,7 +30,7 @@ class Minecraft(commands.Cog):
             self.sizes = _json["sizes"]
 
     def cog_unload(self):
-        self.bot.loop.create_task(self.ses.stop())
+        self.bot.loop.create_task(self.ses.close())
 
     @commands.command(name="mcping") # Pings a java edition minecraft server
     async def mc_ping(self, ctx, *, server: str):
