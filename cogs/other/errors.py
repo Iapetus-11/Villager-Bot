7117a324496708439e7d136c8968bf1508f55bf9
Iapetus-11 <olimdahacker@gmail.com>
2020-04-27 13:56:54 -0400

    maybe this will work?

--- cogs/other/errors.py
+++ cogs/other/errors.py
@@ -32,7 +32,7 @@ class Errors(commands.Cog):
 
         if isinstance(e, commands.CheckAnyFailure):
             for error in e.errors:
-                if error is commands.MissingPermissions:
+                if isinstance(commands.MissingPermissions, error):
                     await self.send(ctx, "Nice try stupid, but you don't have the permissions to do that.")
                     return
 
