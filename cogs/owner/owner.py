21d816c3150e5f597a382d494b65154e34bfb028
Iapetus-11 <olimdahacker@gmail.com>
2020-05-08 10:55:05 -0400

    top ten?

--- cogs/owner/owner.py
+++ cogs/owner/owner.py
@@ -402,6 +402,13 @@ class Owner(commands.Cog):
         mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"Memory in use: ``{round(mem*.001*1.04858, 2)} Megabytes``"))
 
+    @commands.command(name="topten", aliases=["toptenguilds"])
+    @commands.is_owner()
+    async def top_ten(self, ctx):
+        gds = self.bot.guilds
+        gds.sort(reverse=True, key=guild.member_count)
+        await ctx.send(gds[0])
+
 
 def setup(bot):
     bot.add_cog(Owner(bot))
