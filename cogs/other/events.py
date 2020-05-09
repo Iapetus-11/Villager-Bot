acf9468778987188e1f5e1d486734117defbe105
Iapetus-11 <olimdahacker@gmail.com>
2020-05-09 16:55:41 -0400

    uncomplicate vote event

--- cogs/other/events.py
+++ cogs/other/events.py
@@ -18,7 +18,8 @@ class Events(commands.Cog):
         with open("data/keys.json", "r") as k:
             keys = json.load(k)
 
-        self.dblpy = dbl.DBLClient(self.bot, keys["dblpy"], webhook_path="/dblwebhook", webhook_auth=keys["dblpy2"], webhook_port=5000, autopost=True)
+        self.dblpy = dbl.DBLClient(self.bot, keys["dblpy"], webhook_path="/dblwebhook", webhook_auth=keys["dblpy2"],
+                                   webhook_port=5000, autopost=True)
 
         self.logger = logging.getLogger("Events")
         self.logger.setLevel(logging.INFO)
@@ -50,44 +51,33 @@ class Events(commands.Cog):
         self.logger.info(f"\u001b[32;1m {user_id} VOTED ON TOP.GG \u001b[0m")
         user = self.bot.get_user(user_id)
         if user is not None:
-            await self.bot.get_channel(682195105784004610).send(f":tada::tada: {discord.utils.escape_markdown(user.display_name)} has voted! :tada::tada:")
-            if choice([True, False]):
-                for c in self.g.items:
-                    if randint(0, ceil(c[2]/2)) == 1:
-                        e = "<:emerald:653729877698150405>"
-                        a = "a"
-                        for vowel in ["a", "e", "i", "o", "u"]:
-                            if c[0].startswith(vowel):
-                                a = "an"
-                        try:
-                            await user.send(choice([f"You've received {a} {c[0]} (Worth {c[1]}{e}) for voting for Villager Bot!",
-                                                   f"You've received {a} {c[0]} (Worth {c[1]}{e}) because you voted for Villager Bot!",]))
-                        except discord.errors.Forbidden:
-                            pass
-                        await self.db.add_item(user.id, c[0], 1, c[1])
-                        return
-            multi = 1 # cause easter, normally is 1
+            await self.bot.get_channel(682195105784004610).send(
+                f":tada::tada: {discord.utils.escape_markdown(user.display_name)} has voted! :tada::tada:")
+            multi = 1  # cause easter, normally is 1
             if await self.dblpy.get_weekend_status():
-                multi *= 2 # normally is 2
+                multi = 2  # normally is 2
             messages = ["You have been awarded {0}<:emerald:653729877698150405> for voting for Villager Bot!",
                         "You have received {0}<:emerald:653729877698150405> for voting for Villager Bot!",
                         "You have received {0}<:emerald:653729877698150405> because you voted for Villager Bot!"]
             try:
-                await user.send(embed=discord.Embed(color=discord.Color.green(), description=choice(messages).format(32*multi)))
+                await user.send(
+                    embed=discord.Embed(color=discord.Color.green(), description=choice(messages).format(32 * multi)))
             except discord.errors.Forbidden:
                 pass
             await self.db.set_balance(user_id, await self.db.get_balance(user_id) + (32 * multi))
         else:
-            await self.bot.get_channel(682195105784004610).send(":tada::tada: An unknown user voted for the bot! :tada::tada:")
+            await self.bot.get_channel(682195105784004610).send(
+                ":tada::tada: An unknown user voted for the bot! :tada::tada:")
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         await asyncio.sleep(1)
         if guild.system_channel is not None:
             try:
-                await guild.system_channel.send(embed=discord.Embed(color=discord.Color.green(), description="Hey ya'll, type ``!!help`` to get started with Villager Bot!\n\n"
-                                                                                                         "Want to receive updates, report a bug, or make suggestions? "
-                                                                                                         "Join the official [support server](https://discord.gg/39DwwUV)!"))
+                await guild.system_channel.send(embed=discord.Embed(color=discord.Color.green(),
+                                                                    description="Hey ya'll, type ``!!help`` to get started with Villager Bot!\n\n"
+                                                                                "Want to receive updates, report a bug, or make suggestions? "
+                                                                                "Join the official [support server](https://discord.gg/39DwwUV)!"))
             except discord.errors.Forbidden:
                 pass
 
