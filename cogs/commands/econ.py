19ec3cf8e753a5f40553d4aabe78cea1f7c21a0c
Iapetus-11 <olimdahacker@gmail.com>
2020-05-14 13:29:01 -0400

    fix

--- cogs/commands/econ.py
+++ cogs/commands/econ.py
@@ -228,7 +228,7 @@ class Econ(commands.Cog):
             shop_item = self.g.shop_items.get(item) # Used .get() bc it will return None instead of causing a key error
             if shop_item is not None:
                 if their_bal >= shop_item[0]:
-                    db_item = await self.db.get_item(ctx.author.id, "Fishing Rod")
+                    db_item = await self.db.get_item(ctx.author.id, item)
                     if db_item is not None:
                         item_count = db_item[1]
                     else:
