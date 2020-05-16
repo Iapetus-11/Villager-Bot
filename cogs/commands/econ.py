afe0a3dbdb158cdc65aab5e9079190ff752cb8b2
Iapetus11 <olimdahacker@gmail.com>
2020-05-16 18:15:32 -0400

    bruhmium

--- cogs/commands/econ.py
+++ cogs/commands/econ.py
@@ -18,7 +18,7 @@ class Econ(commands.Cog):
         if ctx.author.id in self.who_is_mining.keys():
             self.who_is_mining[ctx.author.id] += 1
             if self.who_is_mining[ctx.author.id] >= 101:
-                prob = f"{x}+{y}"
+                prob = f"{randint(0, 25)}+{randint(0, 25)}"
                 prob = [prob, str(eval(prob))]
                 await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"Please solve this problem to continue: ``{prob[0]}``"))
                 try:
