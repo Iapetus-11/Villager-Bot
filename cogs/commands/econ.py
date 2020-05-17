a094126796c6e0fcee5f2ae357f9769b52a65e82
Iapetus11 <olimdahacker@gmail.com>
2020-05-17 00:45:54 -0400

    stfff

--- cogs/commands/econ.py
+++ cogs/commands/econ.py
@@ -267,7 +267,7 @@ class Econ(commands.Cog):
                         await self.db.add_item(ctx.author.id, shop_item[2][0], 1, shop_item[2][1])
                         await self.db.set_balance(ctx.author.id, their_bal - shop_item[0])
                         if i == amount - 1:
-                            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"You have purchased {amount}x **{shop_item[2][0]}**! (You now have {item_count})"))
+                            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"You have purchased {amount}x **{shop_item[2][0]}**! (You now have {item_count+1})"))
                             return
                     else:
                         if i < amount:
