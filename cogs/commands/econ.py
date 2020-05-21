f3f2742c82b7bb7261f7d7302862b77ca193d019
Iapetus-11 <olimdahacker@gmail.com>
2020-05-21 15:54:52 -0400

    stuffs

--- cogs/commands/econ.py
+++ cogs/commands/econ.py
@@ -214,7 +214,7 @@ class Econ(commands.Cog):
 
         item = _item.lower()
 
-        if (item.startswith("max") or item.startswith("all")) and self.g.shop_items.get(item) is not None:
+        if (item.startswith("max") or item.startswith("all")) and self.g.shop_items.get(item.replace("max ", "").replace("all ", "")) is not None:
             amount = floor((await self.db.get_balance(ctx.author.id))/self.g.shop_items.get(item)[0])
             item = item.replace("max ", "").replace("all ", "")
         else:
