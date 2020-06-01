bbc15d07d90406e5fc2b3a881ee8702d54250cab
Iapetus-11 <olimdahacker@gmail.com>
2020-06-01 13:17:42 -0400

    execute the polish /s

--- cogs/other/mobspawning.py
+++ cogs/other/mobspawning.py
@@ -6,6 +6,7 @@ class MobSpawning(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Do something with @on_message_event, maybe have dict with counters or somethin idk do algo depends on guild members there
 
 def setup(bot):
     bot.add_cog(MobSpawning(bot))
