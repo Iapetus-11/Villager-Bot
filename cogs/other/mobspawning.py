bd88305040401643a4e5925b70228df752acd88f
Iapetus-11 <olimdahacker@gmail.com>
2020-06-02 12:58:42 -0400

    urmom

--- cogs/other/mobspawning.py
+++ cogs/other/mobspawning.py
@@ -7,9 +7,12 @@ class MobSpawning(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        # "mobname": [img_url, health, ]
-        self.mobs = {"zombie": []}
+        # "mobname": [actualname, health, ]
+        murl = "http://olimone.ddns.net/images/mob_spawns/"
+        self.mobs = {"zombie": ["zombie", 20, murl+"zombie.png"], "spider": ["spider", 16, murl+"spider.png"], "skeleton": ["skeleton", 20, murl+"skeleton.png"],
+                     "creeper": ["creeper", 20, murl+"creeper.png"], "cave_spider": ["cave spider", 12, murl+"cave_spider.png"]}
 
+    # also have random pillager events where server is ransacked
     async def spawn_event(self, gid): # Fuck me in the balls, wait don't how is that even possible?!
         mob = choice(list(self.mobs))
 
