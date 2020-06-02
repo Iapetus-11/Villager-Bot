f3e3b4eb2d77aa616b5fb288c5b4bbcd8639b81d
Iapetus-11 <olimdahacker@gmail.com>
2020-06-02 15:48:57 -0400

    aaa

--- cogs/other/mobspawning.py
+++ cogs/other/mobspawning.py
@@ -8,6 +8,8 @@ class MobSpawning(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        self.db = self.bot.get_cog("Database")
+
         self.do_event = []
 
         # "mobname": [actualname, health, ]
