645853952f8fdc6720d8ea5e1727a9e0c899f380
Iapetus-11 <olimdahacker@gmail.com>
2020-06-02 21:25:57 -0400

    yep

--- cogs/commands/useful.py
+++ cogs/commands/useful.py
@@ -341,10 +341,7 @@ f'**{ctx.prefix}honey** *apparently bees produce honey, who knew it could sell f
                 if letter in problem:
                     await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="That math problem contains invalid characters, please try again."))
                     return
-            try:
-                await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"```{eval(problem)}```"))
-            except Exception:
-                await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="Oops, something went wrong."))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"```{eval(problem)}```"))
         except Exception:
             await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="Oops, something went wrong."))
 
