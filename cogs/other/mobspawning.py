89d17d094fe24551cbc22adccb03cb64647a4150
Iapetus-11 <olimdahacker@gmail.com>
2020-06-02 20:48:02 -0400

    change some shit

--- cogs/other/mobspawning.py
+++ cogs/other/mobspawning.py
@@ -33,7 +33,7 @@ class MobSpawning(commands.Cog):
         mob_key = choice(list(self.mobs))
         mob = self.mobs[mob_key] # LMAO I bet there's a better way to do this but fuck it
 
-        f_embed = discord.Embed(color=discord.Color.green(), title="**"+choice(self.drop_msgs).format(mob[0])+"**", description="Do you want to ``fight`` the mob or ``flee``?")
+        f_embed = discord.Embed(color=discord.Color.green(), title="**"+choice(self.drop_msgs).format(mob[0])+"**", description="Do you want to ``fight`` the mob or ``flee``?") # fight it or u little baby
         f_embed.set_image(url=mob[2])
         f_msg = await ctx.send(embed=f_embed)
         try:
@@ -47,7 +47,7 @@ class MobSpawning(commands.Cog):
                 pass
             await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="You ran out of time! The mob despawned."))
             return
-        if m.content == "flee":
+        if m.content == "flee": # That's right you whiny little shit
             await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"You ran away like {choice(['a little baby', 'a little kid','a little baby screaming mommy', 'a whiny little baby', 'the whiny little kid you are'])}."))
             return
 
@@ -56,18 +56,19 @@ class MobSpawning(commands.Cog):
         hh = ["<:heart_full:717535027604488243>", "<:heart_empty:717535027319144489>"]
         while h_user > 0 and mob[1] > 0:
             h_user = await self.db.get_health(u.id)
-            new_emb = discord.Embed(color=discord.Color.green(), description="Do you want to ``attack`` or ``flee``?")
+            new_emb = discord.Embed(color=discord.Color.green(), title="Do you want to ``attack`` or ``flee``?")
             new_emb.add_field(name=f"**{u.display_name}**", value=await self.db.calc_stat_bar(h_user, 20, 10, hh[0], hh[1]), inline=False) # how tf is this gonna work ya retarded cunt
             new_emb.add_field(name=f"**{mob[0]}**", value=await self.db.calc_stat_bar(mob[1], self.mobs[mob_key][1], self.mobs[mob_key][1]/2, hh[0], hh[1]), inline=False)
             new_emb.set_image(url=mob[2])
-            await f_msg.edit(embed=new_emb)
+            await f_msg.edit(suppress=True)
+            f_msg = await ctx.send(embed=new_emb)
             break
 
     @commands.Cog.listener()
     async def on_ready(self):
         while self.bot.is_ready():
             asyncio.sleep(.05) # idk why this but this?
-            for ctx in self.do_event:
+            for ctx in self.do_event: # ah yes efficiency
                 await self.spawn_event(ctx)
 
 
