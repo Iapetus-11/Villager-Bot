83ca80ec5ffd2207b58fe67fae2935e435179cf4
Iapetus-11 <olimdahacker@gmail.com>
2020-06-06 14:32:52 -0400

    new ~~nips~~ tips

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 tips = ["Did you know you can get emeralds from voting for us on [top.gg](https://top.gg/bot/639498607632056321)?",
         "If you ever need more help, don't forget to check out the [support server](https://discord.gg/39DwwUV)!",
         "Have any suggestions? Use the suggestion channel in our [support server](https://discord.gg/39DwwUV)!",
-        "In need of **emeralds**? Check out the #paid-stuff channel on the [support server](https://discord.gg/39DwwUV)!"]
+        "In need of **emeralds**? Check out the #paid-stuff channel on the [support server](https://discord.gg/39DwwUV)!",
+        "Hey! Check out one of our other bots, [vidio](https://top.gg/bot/689210550680682560), a YouTube sim/roleplay bot!"]
 
 with open("data/keys.json", "r") as k:  # Loads secret keys
     keys = json.load(k)
