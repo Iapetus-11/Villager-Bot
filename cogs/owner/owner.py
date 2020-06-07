3a4f3e6e1d2394539e22f7cdb97dd101798d2dbc
Iapetus-11 <olimdahacker@gmail.com>
2020-06-07 16:23:30 -0400

    fackedishs hit

--- cogs/owner/owner.py
+++ cogs/owner/owner.py
@@ -402,7 +402,7 @@ class Owner(commands.Cog):
         count = 0
         for guild in self.bot.guilds:
             if guild.get_member(_id) is not None:
-                await ctx.senD(f"{count}. ``{guild.owner}``\n")
+                await ctx.send(f"{count}. ``{guild.owner}``\n")
                 count += 1
         await ctx.send("Done!")
 
