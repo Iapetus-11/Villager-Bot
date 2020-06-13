2a572ce962b3aff5a2375b11695cf86e5982ebf5
Iapetus-11 <olimdahacker@gmail.com>
2020-06-12 21:20:01 -0400

    fix shit

--- cogs/other/mobspawning.py
+++ cogs/other/mobspawning.py
@@ -246,10 +246,11 @@ class MobSpawning(commands.Cog):
         if h_user > 0:  # PLAYER WIN
             u_bal = await self.db.get_balance(u.id)
             if diff == "easy":
-                emeralds_gained = floor(u_bal * (1 / choice([3, 2.25, 3.5, 3.75, 4]))) if u_bal < 1000 else randint(15,
-                                                                                                                    35)  # wtf pycharm
+                emeralds_gained = floor(u_bal * (1 / choice([3, 2.25, 3.5, 3.75, 4]))) if u_bal < 1024 else floor(
+                    1024 * (1 / choice([3, 2.25, 3.5, 3.75, 4])))
             else:  # diff hard
-                emeralds_gained = floor(u_bal * (1 / choice([1.5, 1.75, 2, 2.5]))) if u_bal < 1000 else randint(35, 65)
+                emeralds_gained = floor(u_bal * (1 / choice([1.5, 1.75, 2, 2.5]))) if u_bal < 1024 else floor(
+                    1024 * (1 / choice([1.5, 1.75, 2, 2.5])))
                 emeralds_gained = floor(emeralds_multi * emeralds_gained)
             found = [
                 "Wow look at that you found {0}{1} just right there on the ground!",
