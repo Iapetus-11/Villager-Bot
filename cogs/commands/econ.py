3edc58e909614aa9cf91a5e4d9c7ece5f77ed05f
Iapetus-11 <olimdahacker@gmail.com>
2020-06-12 21:39:44 -0400

    stuffs

--- cogs/commands/econ.py
+++ cogs/commands/econ.py
@@ -185,23 +185,24 @@ class Econ(commands.Cog):
                         icon_url="http://172.10.17.177/images/villagerbotsplash1.png")
         shop.add_field(name=f"__**Stone Pickaxe**__ 32{self.emerald}", value=f"``{ctx.prefix}buy stone pickaxe``",
                        inline=True)
+        shop.add_field(name="\uFEFF", value="\uFEFF")
         shop.add_field(name=f"__**Iron Pickaxe**__ 128{self.emerald}", value=f"``{ctx.prefix}buy iron pickaxe``",
                        inline=True)
-        shop.add_field(name="\uFEFF", value="\uFEFF")
         shop.add_field(name=f"__**Gold Pickaxe**__ 512{self.emerald}", value=f"``{ctx.prefix}buy gold pickaxe``",
                        inline=True)
+        shop.add_field(name="\uFEFF", value="\uFEFF")
         shop.add_field(name=f"__**Diamond Pickaxe**__ 2048{self.emerald}", value=f"``{ctx.prefix}buy diamond pickaxe``",
                        inline=True)
-        shop.add_field(name="\uFEFF", value="\uFEFF")
         shop.add_field(name=f"__**Netherite Pickaxe**__ 8192{self.emerald} 4<:netherite_scrap:676974675091521539>",
                        value=f"``{ctx.prefix}buy netherite pickaxe``", inline=False)
         shop.add_field(name=f"__**Stone Sword**__ 32{self.emerald}", value=f"``{ctx.prefix}buy stone sword``",
                        inline=True)
+        shop.add_field(name="\uFEFF", value="\uFEFF")
         shop.add_field(name=f"__**Iron Sword**__ 128{self.emerald}", value=f"``{ctx.prefix}buy iron sword``",
                        inline=True)
-        shop.add_field(name="\uFEFF", value="\uFEFF")
         shop.add_field(name=f"__**Gold Sword**__ 512{self.emerald}", value=f"``{ctx.prefix}buy gold sword``",
                        inline=True)
+        shop.add_field(name="\uFEFF", value="\uFEFF")
         shop.add_field(name=f"__**Diamond Sword**__ 2048{self.emerald}", value=f"``{ctx.prefix}buy diamond sword``",
                        inline=True)
         shop.add_field(name=f"__**Netherite Sword**__ 8192{self.emerald} 6<:netherite_scrap:676974675091521539>",
