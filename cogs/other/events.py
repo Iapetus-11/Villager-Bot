888169d9501fe764c4d3ae5273052fbb948d4c9b
Iapetus11 <olimdahacker@gmail.com>
2020-06-24 18:10:19 -0400

    debugggggg

--- cogs/other/events.py
+++ cogs/other/events.py
@@ -35,6 +35,7 @@ class Events(commands.Cog):
     async def webhook(self):
 
         async def vote_handler(r):
+            print(r.headers.get("X-DBL-Signature"))
             user_id = (await r.json())["id"]
             self.logger.info(f"\u001b[32;1m {user_id} VOTED ON DBL2 \u001b[0m")
             user = self.bot.get_user(user_id)
