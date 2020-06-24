4887f50dbe9037fc1d489b6da8fd4ace88d0b330
Iapetus11 <olimdahacker@gmail.com>
2020-06-24 17:58:31 -0400

    remove debug

--- cogs/other/events.py
+++ cogs/other/events.py
@@ -40,7 +40,6 @@ class Events(commands.Cog):
     async def webhook(self):
 
         async def vote_handler(r):
-            print(r.text)
             user_id = int(await r.json())
             self.logger.info(f"\u001b[32;1m {user_id} VOTED ON DBL2 \u001b[0m")
             user = self.bot.get_user(user_id)
