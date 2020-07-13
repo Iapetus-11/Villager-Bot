6cbc0070988db35d5a69caac6c6f0ac60ee8aaa3
Iapetus-11 <olimdahacker@gmail.com>
2020-07-13 17:25:45 -0400

    faster server pinging

--- cogs/commands/mc.py
+++ cogs/commands/mc.py
@@ -65,7 +65,7 @@ class Minecraft(commands.Cog):
 
         return True, status.players.online, status.latency
 
-    async def unified_mc_ping(self, server_str, _port=None):
+    async def unified_mc_ping(self, server_str, _port=None, _ver=None):
         if ":" in server_str and _port is None:
             split = server_str.split(":")
             ip = split[0]
@@ -79,29 +79,48 @@ class Minecraft(commands.Cog):
         else:
             str_port = f":{port}"
 
-        # ONLY JE servers
-        standard_je_ping_partial = partial(self.standard_je_ping, f"{ip}{str_port}")
-        with concurrent.futures.ThreadPoolExecutor() as pool:
-            s_je_online, s_je_players, s_je_latency = await self.bot.loop.run_in_executor(pool,
-                                                                                          standard_je_ping_partial)
-        if s_je_online:
-            return {"online": True, "player_count": s_je_players, "ping": s_je_latency, "version": "Java Edition"}
+        if _ver == "je":
+            # ONLY JE servers
+            standard_je_ping_partial = partial(self.standard_je_ping, f"{ip}{str_port}")
+            with concurrent.futures.ThreadPoolExecutor() as pool:
+                s_je_online, s_je_players, s_je_latency = await self.bot.loop.run_in_executor(pool,
+                                                                                              standard_je_ping_partial)
+            if s_je_online:
+                return {"online": True, "player_count": s_je_players, "ping": s_je_latency, "version": "Java Edition"}
 
-        # JE & PocketMine
-        resp = await self.ses.get(f"https://api.mcsrvstat.us/2/{ip}{str_port}")
-        jj = await resp.json()
-        if jj.get("online"):
-            return {"online": True, "player_count": jj.get("players", {}).get("online", 0), "ping": None,
-                    "version": jj.get("software")}
+            return {"online": False, "player_count": 0, "ping": None, "version": None}
+        elif _ver == "api":
+            # JE & PocketMine
+            resp = await self.ses.get(f"https://api.mcsrvstat.us/2/{ip}{str_port}")
+            jj = await resp.json()
+            if jj.get("online"):
+                return {"online": True, "player_count": jj.get("players", {}).get("online", 0), "ping": None,
+                        "version": jj.get("software")}
+            return {"online": False, "player_count": 0, "ping": None, "version": None}
+        elif _ver == "be":
+            # Vanilla MCPE / Bedrock Edition (USES RAKNET)
+            vanilla_pe_ping_partial = partial(self.vanilla_pe_ping, ip, port if port is not None else 19132)
+            with concurrent.futures.ThreadPoolExecutor() as pool:
+                pe_online, pe_p_count = await self.bot.loop.run_in_executor(pool, vanilla_pe_ping_partial)
+            if pe_online:
+                return {"online": True, "player_count": pe_p_count, "ping": None, "version": "Vanilla Bedrock Edition"}
+            return {"online": False, "player_count": 0, "ping": None, "version": None}
+        else:
+            tasks = [
+                self.bot.loop.create_task(self.unified_mc_ping(ip, port, "je")),
+                self.bot.loop.create_task(self.unified_mc_ping(ip, port, "api")),
+                self.bot.loop.create_Task(self.unified_mc_ping(ip, port, "be"))
+            ]
 
-        # Vanilla MCPE / Bedrock Edition (USES RAKNET)
-        vanilla_pe_ping_partial = partial(self.vanilla_pe_ping, ip, port if port is not None else 19132)
-        with concurrent.futures.ThreadPoolExecutor() as pool:
-            pe_online, pe_p_count = await self.bot.loop.run_in_executor(pool, vanilla_pe_ping_partial)
-        if pe_online:
-            return {"online": True, "player_count": pe_p_count, "ping": None, "version": "Vanilla Bedrock Edition"}
+            for task in tasks:
+                while not task.done():
+                    await asyncio.sleep(.05)
 
-        return {"online": False, "player_count": 0, "ping": None, "version": None}
+            for task in tasks:
+                if task.result().get("online") is True:
+                    return task.result()
+
+            return {"online": False, "player_count": 0, "ping": None, "version": None}
 
     @commands.command(name="mcping")
     async def mc_ping(self, ctx, server: str, port: int = None):
@@ -133,22 +152,26 @@ class Minecraft(commands.Cog):
         if uuid is None:
             await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="That player doesn't exist!"))
             return
-        response = await self.ses.get(f"https://sessionserver.mojang.com/session/minecraft/profile/{uuid}?unsigned=false")
+        response = await self.ses.get(
+            f"https://sessionserver.mojang.com/session/minecraft/profile/{uuid}?unsigned=false")
         content = json.loads(await response.text())
         if "error" in content:
             if content["error"] == "TooManyRequestsException":
                 await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="Hey! Slow down!"))
                 return
         if len(content["properties"]) == 0:
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="This user's skin can't be stolen for some reason..."))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(),
+                                               description="This user's skin can't be stolen for some reason..."))
             return
         undec = base64.b64decode(content["properties"][0]["value"])
         try:
             url = json.loads(undec)["textures"]["SKIN"]["url"]
         except Exception:
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="An error occurred while fetching that skin!"))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(),
+                                               description="An error occurred while fetching that skin!"))
             return
-        skin_embed = discord.Embed(color=discord.Color.green(), description=f"{gamertag}'s skin\n[**[Download]**]({url})")
+        skin_embed = discord.Embed(color=discord.Color.green(),
+                                   description=f"{gamertag}'s skin\n[**[Download]**]({url})")
         skin_embed.set_thumbnail(url=url)
         skin_embed.set_image(url=f"https://mc-heads.net/body/{gamertag}")
         await ctx.send(embed=skin_embed)
@@ -157,9 +180,10 @@ class Minecraft(commands.Cog):
     @commands.cooldown(1, 1, commands.BucketType.user)
     async def get_uuid(self, ctx, *, gamertag: str):
         r = await self.ses.post("https://api.mojang.com/profiles/minecraft", json=[gamertag])
-        j = json.loads(await r.text()) # [0]['id']
+        j = json.loads(await r.text())  # [0]['id']
         if not j:
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="That user could not be found."))
+            await ctx.send(
+                embed=discord.Embed(color=discord.Color.green(), description="That user could not be found."))
             return
         await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"{gamertag}: ``{j[0]['id']}``"))
 
@@ -174,59 +198,65 @@ class Minecraft(commands.Cog):
             await ctx.send(embed=discord.Embed(color=discord.Color.green(), description="That player doesn't exist!"))
             return
         j = json.loads(await response.text())
-        name = j[len(j)-1]["name"]
+        name = j[len(j) - 1]["name"]
         await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"{uuid}: ``{name}``"))
 
     @commands.command(name="mcsales", aliases=["minecraftsales"])
     @commands.cooldown(1, 1, commands.BucketType.user)
     async def mc_sales(self, ctx):
-        r = await self.ses.post("https://api.mojang.com/orders/statistics", json={"metricKeys": ["item_sold_minecraft", "prepaid_card_redeemed_minecraft"]})
+        r = await self.ses.post("https://api.mojang.com/orders/statistics",
+                                json={"metricKeys": ["item_sold_minecraft", "prepaid_card_redeemed_minecraft"]})
         j = json.loads(await r.text())
-        await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"**{j['total']}** total Minecraft copies sold, **{round(j['saleVelocityPerSeconds'], 3)}** copies sold per second."))
+        await ctx.send(embed=discord.Embed(color=discord.Color.green(),
+                                           description=f"**{j['total']}** total Minecraft copies sold, **{round(j['saleVelocityPerSeconds'], 3)}** copies sold per second."))
 
     @commands.command(name="randomserver", aliases=["randommc", "randommcserver", "mcserver", "minecraftserver"])
     async def random_mc_server(self, ctx):
         s = choice(self.g.mcServers)
         try:
-            online = MinecraftServer.lookup(s['ip']+":"+str(s['port'])).status()
+            online = MinecraftServer.lookup(s['ip'] + ":" + str(s['port'])).status()
             stat = "<:online:692764696075304960>"
         except Exception:
             stat = "<:offline:692764696431951872>"
-        await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"{stat} \uFEFF ``{s['ip']}:{s['port']}`` {s['version']} ({s['type']})\n{s['note']}"))
+        await ctx.send(embed=discord.Embed(color=discord.Color.green(),
+                                           description=f"{stat} \uFEFF ``{s['ip']}:{s['port']}`` {s['version']} ({s['type']})\n{s['note']}"))
 
     @commands.command(name="buildidea", aliases=["idea"])
     async def build_idea(self, ctx):
         if choice([True, False]):
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"{choice(self.first)} {choice(self.prenouns)}{choice(['!', ''])}"))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(),
+                                               description=f"{choice(self.first)} {choice(self.prenouns)}{choice(['!', ''])}"))
         else:
-            await ctx.send(embed=discord.Embed(color=discord.Color.green(), description=f"{choice(self.first)} a {choice(self.sizes)}, {choice(self.colors)} {choice(self.nouns)}{choice(['!', ''])}"))
+            await ctx.send(embed=discord.Embed(color=discord.Color.green(),
+                                               description=f"{choice(self.first)} a {choice(self.sizes)}, {choice(self.colors)} {choice(self.nouns)}{choice(['!', ''])}"))
 
     @commands.command(name="colorcodes", aliases=["mccolorcodes", "colors", "cc"])
     async def mc_color_codes(self, ctx):
-        embed = discord.Embed(color=discord.Color.green(), description="Text in Minecraft can be formatted using different codes and\nthe section (``§``) sign.")
+        embed = discord.Embed(color=discord.Color.green(),
+                              description="Text in Minecraft can be formatted using different codes and\nthe section (``§``) sign.")
         embed.set_author(name="Minecraft Formatting Codes")
         embed.add_field(name="Color Codes", value="<:red:697541699706028083> **Red** ``§c``\n"
-                        "<:yellow:697541699743776808> **Yellow** ``§e``\n"
-                        "<:green:697541699316219967> **Green** ``§a``\n"
-                        "<:aqua:697541699173613750> **Aqua** ``§b``\n"
-                        "<:blue:697541699655696787> **Blue** ``§9``\n"
-                        "<:light_purple:697541699546775612> **Light Purple** ``§d``\n"
-                        "<:white:697541699785719838> **White** ``§f``\n"
-                        "<:gray:697541699534061630> **Gray** ``§7``\n")
+                                                  "<:yellow:697541699743776808> **Yellow** ``§e``\n"
+                                                  "<:green:697541699316219967> **Green** ``§a``\n"
+                                                  "<:aqua:697541699173613750> **Aqua** ``§b``\n"
+                                                  "<:blue:697541699655696787> **Blue** ``§9``\n"
+                                                  "<:light_purple:697541699546775612> **Light Purple** ``§d``\n"
+                                                  "<:white:697541699785719838> **White** ``§f``\n"
+                                                  "<:gray:697541699534061630> **Gray** ``§7``\n")
         embed.add_field(name="Color Codes", value="<:dark_red:697541699488055426> **Dark Red** ``§4``\n"
-                        "<:gold:697541699639050382> **Gold** ``§6``\n"
-                        "<:dark_green:697541699500769420> **Dark Green** ``§2``\n"
-                        "<:dark_aqua:697541699475472436> **Dark Aqua** ``§3``\n"
-                        "<:dark_blue:697541699488055437> **Dark Blue** ``§1``\n"
-                        "<:dark_purple:697541699437592666> **Dark Purple** ``§5``\n"
-                        "<:dark_gray:697541699471278120> **Dark Gray** ``§8``\n"
-                        "<:black:697541699496444025> **Black** ``§0``\n")
+                                                  "<:gold:697541699639050382> **Gold** ``§6``\n"
+                                                  "<:dark_green:697541699500769420> **Dark Green** ``§2``\n"
+                                                  "<:dark_aqua:697541699475472436> **Dark Aqua** ``§3``\n"
+                                                  "<:dark_blue:697541699488055437> **Dark Blue** ``§1``\n"
+                                                  "<:dark_purple:697541699437592666> **Dark Purple** ``§5``\n"
+                                                  "<:dark_gray:697541699471278120> **Dark Gray** ``§8``\n"
+                                                  "<:black:697541699496444025> **Black** ``§0``\n")
         embed.add_field(name="Formatting Codes", value="<:bold:697541699488186419> **Bold** ``§l``\n"
-                        "<:strikethrough:697541699768942711> ~~Strikethrough~~ ``§m``\n"
-                        "<:underline:697541699806953583> __Underline__ ``§n``\n"
-                        "<:italic:697541699152379995> *Italic* ``§o``\n"
-                        "<:obfuscated:697541699769204736> ||Obfuscated|| ``§k``\n"
-                        "<:reset:697541699697639446> Reset ``§r``\n")
+                                                       "<:strikethrough:697541699768942711> ~~Strikethrough~~ ``§m``\n"
+                                                       "<:underline:697541699806953583> __Underline__ ``§n``\n"
+                                                       "<:italic:697541699152379995> *Italic* ``§o``\n"
+                                                       "<:obfuscated:697541699769204736> ||Obfuscated|| ``§k``\n"
+                                                       "<:reset:697541699697639446> Reset ``§r``\n")
         await ctx.send(embed=embed)
 
 
