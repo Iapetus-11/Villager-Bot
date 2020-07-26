42096a76d9b49c02a95bb7e771fffea862173ccf
Iapetus11 <olimdahacker@gmail.com>
2020-07-26 11:54:09 -0400

    make the version field inline if ps_list

--- cogs/commands/mc.py
+++ cogs/commands/mc.py
@@ -153,11 +153,11 @@ class Minecraft(commands.Cog):
 
         if ps_list is None:
             embed.add_field(name="Players Online", value=status.get("player_count"))
-
-        embed.add_field(name="Latency/Ping", value=ping if ping != "None" and ping is not None else "Not Available")
-        embed.add_field(name="Version", value=status.get('version'), inline=False)
-
-        if ps_list is not None:
+            embed.add_field(name="Latency/Ping", value=ping if ping != "None" and ping is not None else "Not Available")
+            embed.add_field(name="Version", value=status.get('version'), inline=False)
+        else:
+            embed.add_field(name="Latency/Ping", value=ping if ping != "None" and ping is not None else "Not Available")
+            embed.add_field(name="Version", value=status.get('version'))
             ps_list_cut = ps_list[:20]
             if len(ps_list_cut) == 0:
                 ps_list_cut.append("No players online.")
