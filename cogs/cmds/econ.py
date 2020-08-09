81d2d7ced3b881d4449daa72b6a28108d98cd671
Iapetus11 <olimdahacker@gmail.com>
2020-08-08 20:11:16 -0400

    add total wealth to bal command

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -18,8 +18,13 @@ class Econ(commands.Cog):
 
         db_user = await self.db.fetch_user(u.id)
 
+        u_items = await self.db.fetch_items(u.id)
+        total_wealth = db_user['emeralds'] + db_user['vault_bal'] * 9 + sum([u_it['sell_price'] + u_it['item_amount'] for u_it in u_items])
+
         embed = discord.Embed(color=self.bot.cc)
         embed.set_author(name=f'{u.display_name}\'s emeralds', icon_url=u.avatar_url_as())
+        
+        embed.description = f'Total Wealth: {total_wealth}{self.bot.custom_emojis["emerald"]}'
 
         embed.add_field(name='Pocket', value=f'{db_user["emeralds"]}{self.bot.custom_emojis["emerald"]}')
         embed.add_field(name='Vault', value=f'{db_user["vault_bal"]}{self.bot.custom_emojis["emerald_block"]}/{db_user["vault_max"]}')
