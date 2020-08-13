594dfbf4351cee875d34f8f539c3d877928d3ee5
Iapetus11 <olimdahacker@gmail.com>
2020-08-12 21:44:17 -0400

    utilize new format_required in buy command

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -412,7 +412,7 @@ class Econ(commands.Cog):
             await self.db.add_item(ctx.author.id, shop_item[3][0], shop_item[3][1], amount)
 
             await self.bot.send(ctx, f'You have bought {amount}x **{shop_item[3][0]}**!'
-            f'for {amount * shop_item[1]}{self.bot.custom_emojis["emerald"]} (You have {amount + db_item["item_amount"]} total)')
+            f'for {await self.db.format_required(shop_item, amount)} (You have {amount + db_item["item_amount"]} total)')
 
             if shop_item[3][0] == 'Rich Person Trophy':
                 await self.db.rich_trophy_wipe(ctx.author.id)
