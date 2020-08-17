286a052db379da6cde65bd97e1c3d5803583aa40
Shamus Schneider <67710582+Sh-wayz@users.noreply.github.com>
2020-08-17 15:38:49 -0400

    Added OwO command

--- cogs/commands/fun.py
+++ cogs/commands/fun.py
@@ -144,6 +144,12 @@ class Fun(commands.Cog):
             return
         await ctx.send(clapped)
 
+    @commands.command(name='owo', aliases=['owofy'])
+    async def owofy_text(self, ctx, *, text):
+      """Make any string more cringe"""
+        owos = [' øwø', ' ◕w◕', ' ♥w♥', ' (owo)', ' ◔w◔', ' 𝓞𝔀𝓞', ' OɯO', ' (。O ω O。)', ' (。O⁄ ⁄ω⁄ ⁄ O。)', ' ♥(。ᅌ ω ᅌ。)', ' (ʘωʘ)', ' ( °ω° )', ' ( ͡o ω ͡o )']
+        text = text.lower().replace('l', 'w').replace('r', 'w')
+        await ctx.send(text + random.choice(owos))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
