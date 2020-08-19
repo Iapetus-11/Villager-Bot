7fab54e7cfce933ae5aeb69c25770ae92ea0e62e
Iapetus11 <olimdahacker@gmail.com>
2020-08-19 17:05:14 -0400

    load owos in bot.py / Data class

--- bot.py
+++ bot.py
@@ -105,6 +105,7 @@ class Data:
 
         self.gamble_sayings = jj['gamble']  # stuff for gamble command
         self.begging_sayings = jj['begging']  # text responses for begging command
+        self.owos = jj['owos']  # text for owofy command
 
         # reverse enchant lang and make it its own lang (unenchantlang)
         self.fun_langs['unenchant'] = {}
