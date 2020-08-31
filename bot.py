3d345cdb315fb334903dead2190f21cd15659ca7
Iapetus-11 <olimdahacker@gmail.com>
2020-08-31 17:14:58 -0400

    comments

--- bot.py
+++ bot.py
@@ -72,11 +72,11 @@ bot.d.cmd_count = 0
 bot.d.msg_count = 0
 bot.d.start_time = None
 
-bot.d.miners = {}
+bot.d.miners = {}  # {user_id: commands}
 bot.d.honey_buckets = None  # list of cooldowns for honey command (econ cog)
 bot.d.mining.pickaxes = reversed(list(bot.d.mining.yields_pickaxes))  # get list of pickaxe types from best to worst
 bot.d.findables = bot.d.special_findables + bot.d.default_findables
-bot.d.pillagers = {}
+bot.d.pillagers = {}  # {user_id: daily_pillages}
 bot.d.potions = {}  # {user_id: [potion, potion]}
 
 bot.d.splash_logo = 'http://172.10.17.177/images/villagerbotsplash1.png'
