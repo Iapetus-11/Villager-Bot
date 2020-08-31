3866e220cf3b6422e8faac7fb53f78dce0a0db2b
Iapetus-11 <olimdahacker@gmail.com>
2020-08-31 13:41:12 -0400

    add pillagers dict to bot.d

--- bot.py
+++ bot.py
@@ -72,10 +72,11 @@ bot.d.cmd_count = 0
 bot.d.msg_count = 0
 bot.d.start_time = None
 
-bot.d.miners = []
+bot.d.miners = {}
 bot.d.honey_buckets = None  # list of cooldowns for honey command (econ cog)
 bot.d.mining.pickaxes = reversed(list(bot.d.mining.yields_pickaxes))  # get list of pickaxe types from best to worst
 bot.d.findables = bot.d.special_findables + bot.d.default_findables
+bot.d.pillagers = {}
 
 bot.d.splash_logo = 'http://172.10.17.177/images/villagerbotsplash1.png'
 bot.d.support = 'https://discord.gg/39DwwUV'
