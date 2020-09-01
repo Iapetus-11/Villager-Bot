0ea3123bcb5bbc70f7359564389c3226eba73d6f
Iapetus-11 <olimdahacker@gmail.com>
2020-08-31 21:44:53 -0400

    call math_problem not problem() in mine command

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -623,7 +623,7 @@ class Econ(commands.Cog):
 
     @commands.command(name='mine', aliases=['mein'])
     async def mine(self, ctx):
-        if not self.problem(ctx): return
+        if not self.math_problem(ctx): return
 
         db_user = await self.db.fetch_user(ctx.author.id)
         pickaxe = await self.db.fetch_pickaxe(ctx.author.id)
