ac27b22464948f6fab97c28329295f6706543363
Iapetus-11 <olimdahacker@gmail.com>
2020-09-02 15:04:21 -0400

    add wood pickaxe to user in fetch_pickaxe if user doesn't have a pickaxe

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -90,6 +90,7 @@ class Database(commands.Cog):
             if pickaxe in items_names:
                 return pickaxe
             else:
+                await self.add_item(uid, 'Wood Pickaxe', 0, 1)
                 return self.bot.d.mining.pickaxes[-1]
 
     async def fetch_sword(self, uid):
