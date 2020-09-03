9fc3710d2579a08c4535075f48b730188a5b462d
Iapetus-11 <olimdahacker@gmail.com>
2020-09-02 23:26:09 -0400

    debug

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -350,6 +350,9 @@ class Econ(commands.Cog):
 
         db_item = await self.db.fetch_item(ctx.author.id, shop_item[3][0])
 
+        await ctx.send(db_item)
+        
+
         if shop_item[2] == "db_item_count < 1":
             amount = 1
 
