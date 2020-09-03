b206e6dc2b36fd966ce57049625c135fafc6833f
Iapetus-11 <olimdahacker@gmail.com>
2020-09-03 12:54:19 -0400

    use db_item for getting name of item instead of item var in /give

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -493,7 +493,7 @@ class Econ(commands.Cog):
                 return
 
             await self.db.remove_item(ctx.author.id, item, amount)
-            await self.db.add_item(user.id, item, db_item['sell_price'], amount)
+            await self.db.add_item(user.id, db_item['name'], db_item['sell_price'], amount)
 
             await self.bot.send(ctx, f'{ctx.author.mention} gave {amount}x **{db_item["name"]}** to {user.mention}')
 
