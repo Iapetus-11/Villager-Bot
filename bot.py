bd876cea757ad4381e49cb2ea53cb548194aba56
Iapetus-11 <olimdahacker@gmail.com>
2020-09-08 22:33:39 -0400

    load useful cog

--- bot.py
+++ bot.py
@@ -91,6 +91,7 @@ for key in list(bot.d.fun_langs.enchant):  # reverse the enchant lang to get the
 bot.cog_list = [  # list of cogs which are to be loaded in the bot
     'cogs.core.database',
     'cogs.core.events',
+    'cogs.cmds.useful',
     'cogs.cmds.mc',
     'cogs.cmds.mod',
     'cogs.cmds.fun',
