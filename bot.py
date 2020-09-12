59f1337397b3c76e845085b6b069dd5d6319dea5
Iapetus-11 <olimdahacker@gmail.com>
2020-09-12 12:11:42 -0400

    fix get_prefix coroutine

--- bot.py
+++ bot.py
@@ -21,8 +21,6 @@ with open("data/config.json", "r") as c:  # load config
 
 
 async def get_prefix(_bot, ctx):  # async function to fetch a prefix from the database
-    return '/'
-
     if DEBUG:
         return '/'
 
