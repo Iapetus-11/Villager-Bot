86e0c61a60fea61ded1a5e61f28bc342331e74aa
Iapetus-11 <olimdahacker@gmail.com>
2020-09-16 21:24:47 -0400

    fix text when you find an emerald while mining

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -649,7 +649,7 @@ class Econ(commands.Cog):
 
             await self.db.balance_add(ctx.author.id, found)
 
-            await self.bot.send(ctx, ctx.l.econ.mine.found_emeralds.format(ctx.l.econ.mine.actions, found, self.d.emojis.emerald))
+            await self.bot.send(ctx, ctx.l.econ.mine.found_emeralds.format(random.choice(ctx.l.econ.mine.actions), found, self.d.emojis.emerald))
 
     @commands.command(name='pillage')
     async def pillage(self, ctx, victim: discord.User):
