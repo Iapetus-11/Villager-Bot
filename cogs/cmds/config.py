54820aac8b45c307e25964c65e9052b90a9b88cf
Iapetus11 <olimdahacker@gmail.com>
2020-09-19 12:38:08 -0400

    add base config command group

--- cogs/cmds/config.py
+++ cogs/cmds/config.py
@@ -10,5 +10,18 @@ class Config(commands.Cog):
 
         self.db = self.bot.db
 
+    @commands.group(name='config', aliases=['settings', 'conf'])
+    async def config(self, ctx):
+        if ctx.invoked_subcommand is None:
+            embed = discord.Embed(color=self.d.cc)
+
+            guild_conf = 'bruh'
+            embed.add_field(name='Server/Guild Configuration', value=guild_conf)
+
+            user_conf = 'bruh2.0'
+            embed.add_field(name='Per User Configuration', value=user_conf)
+
+            await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Config(bot))
