1d2df505f8fcdd842264347c9390ab5510a1936f
Iapetus-11 <olimdahacker@gmail.com>
2020-09-23 09:49:59 -0400

    fix another typo

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -910,7 +910,7 @@ class Econ(commands.Cog):
             else:
                 user = user.display_name
 
-            body += rank_fstr.format(u_place+1, entry[1], user)
+            body += rank_fstr.format(place+1, entry[1], user)
 
         if u_place > 9:
             body += '⋮\n' + rank_fstr.format(u_place, origin_value, self.bot.get_user(origin_uid).display_name)
