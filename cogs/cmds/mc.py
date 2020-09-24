c327afe118114089239e2cd70bca81b606b9b2a9
Iapetus-11 <olimdahacker@gmail.com>
2020-09-24 18:03:58 -0400

    try to fetch another server if server is offline in randommc

--- cogs/cmds/mc.py
+++ cogs/cmds/mc.py
@@ -64,6 +64,9 @@ class Minecraft(commands.Cog):
                 jj = await res.json()
 
         if not jj['success'] or not jj['online']:
+            if ctx.command.name == 'randommc':
+                await self.randommc(ctx)
+                return
             embed = discord.Embed(color=self.d.cc, title=ctx.l.minecraft.mcping.title_offline.format(self.d.emojis.offline, combined))
             await ctx.send(embed=embed)
             return
