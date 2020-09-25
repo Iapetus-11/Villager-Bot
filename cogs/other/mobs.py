b411c283e7370826db6b8231e556b19fccceb83d
Iapetus-11 <olimdahacker@gmail.com>
2020-09-24 22:49:17 -0400

    start work on mob spawning

--- cogs/other/mobs.py
+++ cogs/other/mobs.py
@@ -6,5 +6,14 @@ class Mobs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def spawn_event(self, ctx):
+        pass
+
+    async def spawn_events(self):
+        while True:
+            await asyncio.sleep(.05) # don't fucking remove this or else
+            for ctx in self.d.spawn_queue:
+                self.bot.loop.create_task(self.spawn_event(ctx))
+
 def setup(bot):
     bot.add_cog(Mobs(bot))
