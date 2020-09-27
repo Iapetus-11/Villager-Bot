01473d38a750bee466c6317115f463b4452485f7
Iapetus-11 <olimdahacker@gmail.com>
2020-09-27 16:31:04 -0400

    fix call to fetch_guild in useful.py

--- cogs/cmds/useful.py
+++ cogs/cmds/useful.py
@@ -117,7 +117,7 @@ class Useful(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @commands.command(name='guildinfo', aliases=['server', 'serverinfo'])
+    @commands.command(name='guildinfo', aliases=['server', 'serverinfo', 'guild'])
     @commands.guild_only()
     async def guild_info(self, ctx, gid: int = None):
         if gid == None:
@@ -125,7 +125,7 @@ class Useful(commands.Cog):
         else:
             guild = self.bot.get_guild(gid)
 
-        db_guild = await self.db.fetch_guild()
+        db_guild = await self.db.fetch_guild(gid)
 
         embed = discord.Embed(color=self.d.cc)
         embed.set_author(name=f'{guild.name} Information', icon_url=guild.icon_url)
