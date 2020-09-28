13dcd2874fc6ce44bcdc382eed6ece40b7674903
Iapetus-11 <olimdahacker@gmail.com>
2020-09-28 10:26:52 -0400

    formatting changes / updates

--- cogs/cmds/useful.py
+++ cogs/cmds/useful.py
@@ -87,9 +87,9 @@ class Useful(commands.Cog):
         embed.add_field(name='Shards', value=str(self.bot.shard_count))
         embed.add_field(name='Total Users', value=str(len(self.bot.users)))
 
-        embed.add_field(name='[More Info]', value=f'[Click Here]({self.d.disbots})')
-        embed.add_field(name='[Website]', value=f'[Click Here]({self.d.website})')
-        embed.add_field(name='[Support]', value=f'[Click Here]({self.d.support})')
+        embed.add_field(name='More Info', value=f'[Click Here]({self.d.disbots})')
+        embed.add_field(name='Website]', value=f'[Click Here]({self.d.website})')
+        embed.add_field(name='Support', value=f'[Click Here]({self.d.support})')
 
         embed.set_author(name='Villager Bot Information', icon_url=self.d.splash_logo)
 
