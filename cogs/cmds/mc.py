3a743432f664d8105f8ed9910924536e2859e7a0
Iapetus-11 <olimdahacker@gmail.com>
2020-10-10 17:51:31 -0400

    /shrug

--- cogs/cmds/mc.py
+++ cogs/cmds/mc.py
@@ -54,7 +54,7 @@ class Minecraft(commands.Cog):
     @commands.command(name='mcimage', aliases=['mcpixelart'])
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def mcpixelart(self, ctx):
-        files = ctx.message.attachments
+        files = ctx.attachments
 
         if len(files) < 1:
             await self.bot.send(ctx, 'You have to upload an image in the same message you use the command.')
@@ -73,11 +73,11 @@ class Minecraft(commands.Cog):
             return
 
         with tempfile.NamedTemporaryFile() as source_tmp:
-            tmp_name = f'{source_tmp.name}' # {img.filename[4:]}
+            tmp_name = f'{source_tmp.name}{img.filename[4:]}'
 
-            source_tmp.write(await img.read(use_cached=True))
+            source_tmp.write(img.read(use_cached=True))
 
-            with concurrent.futures.ThreadPoolExecutor() as pool:
+            with concurrent.futures.ProcessPoolExecutor() as pool:
                 mosaic_gen_partial = functools.partial(mosaic.generate, tmp_name, 1600)
                 raw_img = await self.bot.loop.run_in_executor(pool, mosaic_gen_partial)
 
