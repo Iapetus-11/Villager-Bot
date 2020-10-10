e61c1ce801755002e4c612ae23edc46be93831e4
Iapetus-11 <olimdahacker@gmail.com>
2020-10-10 17:57:45 -0400

    remove extra conversions

--- cogs/cmds/mc.py
+++ cogs/cmds/mc.py
@@ -76,10 +76,7 @@ class Minecraft(commands.Cog):
             mosaic_gen_partial = functools.partial(mosaic.generate, await img.read(use_cached=True), 1600)
             raw_img = await self.bot.loop.run_in_executor(pool, mosaic_gen_partial)
 
-        with tempfile.SpooledTemporaryFile() as tmp:
-            tmp.write(raw_img)
-
-            await ctx.send(file=discord.File(tmp.read()))
+        await ctx.send(file=discord.File(raw_img))
 
 
     @commands.command(name='mcping', aliases=['mcstatus'])
