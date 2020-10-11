0f3e000a1cfaea36a351ce4984f8fca605d57837
Iapetus-11 <olimdahacker@gmail.com>
2020-10-11 17:27:57 -0400

    decrease VARCHAR size for reason in warnings table

--- setup.sql
+++ setup.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS warnings( -- moderation warnings
   uid    bigint, -- receiver of the warning
   gid    bigint, -- guild in which the warning was done
   mod_id bigint, -- moderator who did the warning
-  reason varchar(500) -- reason for the warning, null for no reason
+  reason varchar(250) -- reason for the warning, null for no reason
 );
 
 -- CREATE TABLE IF NOT EXISTS mc_servers( -- used for the !!randommc command
