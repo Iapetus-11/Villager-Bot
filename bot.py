6e3c5a759f8a560cf6aa3a35d950bc80a7d17683
Iapetus-11 <olimdahacker@gmail.com>
2020-10-15 23:51:34 -0400

    make spawnqueue a dict

--- bot.py
+++ bot.py
@@ -125,7 +125,7 @@ bot.d.chuggers = {}  # {user_id: [potion, potion]}
 bot.d.cmd_lb = {}  # {user_id: command_count}
 
 bot.d.pause_econ = {}  # {uid: starttime}
-bot.d.spawn_queue = []  # [ctx, ctx,..]
+bot.d.spawn_queue = {}  # {ctx: starttime}
 
 bot.d.ban_cache = []  # [uid, uid,..]
 bot.d.prefix_cache = {}  # {gid: 'prefix'}
