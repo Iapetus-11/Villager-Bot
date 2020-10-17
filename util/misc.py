6c3678d56a06d5a2ee61ad3c5c8c68887a61e482
Iapetus11 <olimdahacker@gmail.com>
2020-10-17 16:21:33 -0400

    remove self args from functions in misc.py

--- util/misc.py
+++ util/misc.py
@@ -1,5 +1,5 @@
 
-def recursive_update(self, obj, new):  # hOlY FUCKING SHIT this is so big brained I AM A GOD
+def recursive_update(obj, new):  # hOlY FUCKING SHIT this is so big brained I AM A GOD
     if isinstance(obj, dict):
         for k, v in new.items():
             obj[k] = self.recursive_update(obj[k], v)
@@ -12,6 +12,6 @@ def recursive_update(self, obj, new):  # hOlY FUCKING SHIT this is so big braine
     return obj
 
 # Slots should be 10 cause 10 hearts / 2 idk bro I just bsed this function lmao
-def make_stat_bar(self, value, max, slots, full, empty):
+def make_stat_bar(value, max, slots, full, empty):
     occupado = math.floor((value / max) * slots)
     return (full * occupado) + empty * math.floor(slots - occupado)
