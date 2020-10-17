a581f92eb2dccc4515e3c98e3ca83de6bf4446b3
Iapetus11 <olimdahacker@gmail.com>
2020-10-17 16:22:44 -0400

    fix missing import in misc.py

--- util/misc.py
+++ util/misc.py
@@ -1,11 +1,12 @@
+import math
 
 def recursive_update(obj, new):  # hOlY FUCKING SHIT this is so big brained I AM A GOD
     if isinstance(obj, dict):
         for k, v in new.items():
-            obj[k] = self.recursive_update(obj[k], v)
+            obj[k] = recursive_update(obj[k], v)
     elif isinstance(obj, list):
         for i, v in enumerate(new):
-            obj[i] = self.recursive_update(obj[i], v)
+            obj[i] = recursive_update(obj[i], v)
     else:
         return new
 
