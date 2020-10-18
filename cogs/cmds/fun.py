1a68640b2d3ce9c4f0873c3d7eabcdf0fa336caa
Iapetus11 <olimdahacker@gmail.com>
2020-10-18 13:44:48 -0400

    fix meme command

--- cogs/cmds/fun.py
+++ cogs/cmds/fun.py
@@ -55,7 +55,7 @@ class Fun(commands.Cog):
                 meme = cj.classify(await resp.json())
 
         embed = discord.Embed(color=self.d.cc, title=f'{meme.title}', url=meme.permalink)
-        embed.set_footer(text=f'{meme.upvotes} | u/{meme.author}', icon_url=self.bot.get_emoji(self.d.emojis.updoot.split(':')[-1].replace('>', '')).url)
+        embed.set_footer(text=f'{meme.upvotes} | u/{meme.author}', icon_url=self.bot.get_emoji(int(self.d.emojis.updoot.split(':')[-1].replace('>', ''))).url)
         embed.set_image(url=meme.url)
 
         await ctx.send(embed=embed)
