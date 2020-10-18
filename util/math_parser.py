13fc4c476ccffe370650ae6999af02198c91818e
Iapetus11 <olimdahacker@gmail.com>
2020-10-18 12:16:29 -0400

    add math_parser.py

---
+++ util/math_parser.py
@@ -0,0 +1,56 @@
+
+def add(addends):
+    return sum([parse(addend) for addend in addends])
+
+def subtract(subtrahends):
+    subtrahends = [parse(subtrahend) for subtrahend in subtrahends]
+    difference = subtrahends.pop(0)
+
+    for subtrahend in subtrahends:
+        difference -= subtrahend
+
+    return difference
+
+def multiply(multiplicands):
+    multiplicands = [parse(multiplicand) for multiplicand in multiplicands]
+    product = 1
+
+    for multiplicand in multiplicands:
+        product *= multiplicand
+
+    return product
+
+def divide(divisors):
+    divisors = [parse(divisor) for divisor in divisors]
+    quotient = divisors.pop(0)
+
+    for divisor in divisors:
+        quotient /= divisor
+
+    return quotient
+
+def exponentiate(indexes):
+    indexes = [parse(index) for index in indexes]
+    power = indexes.pop(0)
+
+    for index in indexes:
+        power **= index
+
+    return power
+
+def parse(s):
+    # fixes issue of code to split at * messing with ** for exponentiation
+    s = str(s).replace(' ', '').replace('*', 'x').replace('xx', '**').replace('×', 'x').replace('÷', '/')
+
+    # reverse order of operations here because it's resolved bottom up cause it's a tree
+    if len(split := s.split('+')) > 1: return add(split)
+
+    if len(split := s.split('-')) > 1: return subtract(split)
+
+    if len(split := s.split('x')) > 1: return multiply(split)
+
+    if len(split := s.split('/')) > 1: return divide(split)
+
+    if len(split := s.split('**')) > 1: return exponentiate(split)
+
+    return float(s)  # if there's no more parsing that can be done
