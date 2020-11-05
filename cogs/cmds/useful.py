c0ebec311ed58d3d4f8b6c1d0f878f608e888e95
Iapetus-11 <olimdahacker@gmail.com>
2020-11-05 08:53:21 -0500

    fix error in /aliases command

--- cogs/cmds/useful.py
+++ cogs/cmds/useful.py
@@ -110,7 +110,7 @@ class Useful(commands.Cog):
         for alias_group in all_cmds:
             if command in alias_group:
                 if len(alias_group) > 1:
-                    alias_group.pop(command)
+                    alias_group.pop(alias_group.index(command))
                     await self.bot.send(ctx, ctx.l.useful.aliases.aliases.format(command, '`, `'.join(alias_group)))
                     return
 
