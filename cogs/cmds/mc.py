7a8e16ee08c5cb61e50936c0bcfb3242efc115a3
Iapetus-11 <olimdahacker@gmail.com>
2020-11-05 23:14:27 -0500

    add additional mcservers to the Minecraft cog's server_list

--- cogs/cmds/mc.py
+++ cogs/cmds/mc.py
@@ -46,7 +46,7 @@ class Minecraft(commands.Cog):
                     ip = split[16][46:-2].replace('https://', '').replace('http://', '')
                     servers_nice.append((ip, url,))
 
-        self.server_list = list(set(servers_nice)) + [('xenonmc.ml', 'https://discord.gg/VtE3rtV')]*5
+        self.server_list = list(set(servers_nice)) + self.d.additional_mcservers
 
         self.bot.logger.info('finished scraping mc-lists.org')
 
