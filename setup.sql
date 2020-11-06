9f414fd46f7886b9b851756b91dd9a52a0b6ef84
Iapetus-11 <olimdahacker@gmail.com>
2020-11-05 23:07:33 -0500

    update formatting of queries

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS guilds(
+CREATE TABLE IF NOT EXISTS guilds (
   gid        bigint, -- guild id
   prefix     varchar(15), -- prefix in that server
   replies    bool, -- whether or not the should reply to "emeralds" and "villager bot"
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS guilds(
   mcserver   varchar(50) -- default mcserver for the /mcstatus command
 );
 
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
   uid           bigint, -- user id
   emeralds      bigint, -- amount of emeralds user has outside of the vault
   vault_bal     int, -- amount of emerald BLOCKS which are currently in the vault
@@ -17,26 +17,26 @@ CREATE TABLE IF NOT EXISTS users(
   bot_banned    bool -- is banned from using the bot
 );
 
-CREATE TABLE IF NOT EXISTS items(
+CREATE TABLE IF NOT EXISTS items (
   uid          bigint, -- owner of the item
   name         varchar(250), -- name of the item
   sell_price   bigint, -- sell price for ONE of that item
   amount       bigint  -- amount of the item
 );
 
-CREATE TABLE IF NOT EXISTS give_logs(
+CREATE TABLE IF NOT EXISTS give_logs (
   item_name             varchar(250),
   amount                bigint,
   transaction_timestamp bigint
 );
 
-CREATE TABLE IF NOT EXISTS leaderboards(
+CREATE TABLE IF NOT EXISTS leaderboards (
   uid          bigint,
   pillages     bigint, -- number of pillaged emeralds
   mobs_killed  bigint
 );
 
-CREATE TABLE IF NOT EXISTS warnings( -- moderation warnings
+CREATE TABLE IF NOT EXISTS warnings ( -- moderation warnings
   uid    bigint, -- receiver of the warning
   gid    bigint, -- guild in which the warning was done
   mod_id bigint, -- moderator who did the warning
