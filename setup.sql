e67affc9f712a0ba4c4213dae445b4080e8c8696
Iapetus-11 <olimdahacker@gmail.com>
2020-11-05 23:08:53 -0500

    add mc_servers table

--- setup.sql
+++ setup.sql
@@ -43,6 +43,12 @@ CREATE TABLE IF NOT EXISTS warnings ( -- moderation warnings
   reason varchar(250) -- reason for the warning, null for no reason
 );
 
+CREATE TABLE IF NOT EXISTS mc_servers (
+  owner_id bigint, -- discord owner id of the server
+  host varchar(100), -- hostname/ip/address of server
+  link varchar(250) -- learn more link
+)
+
 -- CREATE TABLE IF NOT EXISTS mc_servers( -- used for the !!randommc command
 --   owner_id bigint, -- discord user id of the owner
 --   address  varchar(100), -- address of the server
