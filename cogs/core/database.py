9c0e09bdca0aa9cb2d40b4b0848953565394f109
Iapetus-11 <olimdahacker@gmail.com>
2020-11-12 10:07:48 -0500

    debug

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -49,6 +49,7 @@ class Database(commands.Cog):
                 roles.append(support_guild.get_role(self.d.role_mappings.get('BOP')))
 
             if roles != member.roles:
+                print(roles)
                 await member.edit(roles=roles)
 
     async def fetch_all_botbans(self):
