feedbea1ebf7e668ffa7be2c6ead0eb355abf8ec
Iapetus-11 <olimdahacker@gmail.com>
2020-11-23 16:59:15 -0500

    fix possible error with pp command

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -100,7 +100,8 @@ class Econ(commands.Cog):
         embed.add_field(name='\uFEFF', value='\uFEFF')
         embed.add_field(name=ctx.l.econ.pp.sword, value=(await self.db.fetch_sword(user.id)))
 
-        embed.add_field(name=ctx.l.econ.pp.streak, value=db_user['vote_streak'])
+        vote_streak = db_user['vote_streak']
+        embed.add_field(name=ctx.l.econ.pp.streak, value=(vote_streak if vote_streak else 0))
         voted_today = arrow.utcnow().shift(days=-1) > arrow.get(db_user['streak_time'])
         embed.add_field(name=ctx.l.econ.pp.voted, value=voted_today*ctx.l.econ.pp.yep+ctx.l.econ.pp.nope*voted_today)
 
