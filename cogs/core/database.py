72b851bcfa8f2bf1b6881beab720ecd48739e6ea
Iapetus-11 <olimdahacker@gmail.com>
2020-12-01 08:41:34 -0500

    add missing args in query in set_cmd_usable db helper function

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -106,7 +106,7 @@ class Database(commands.Cog):
     async def set_cmd_usable(self, gid, cmd, usable):
         async with self.db.acquire() as con:
             if usable:
-                await con.execute('DELETE FROM disabled WHERE gid = $1 AND cmd = $2')
+                await con.execute('DELETE FROM disabled WHERE gid = $1 AND cmd = $2', gid, cmd)
             else:
                 await con.execute('INSERT INTO disabled VALUES ($1, $2)', gid, cmd)
 
