e3ae54ad10b58d16fe75ff2c0e713d2a042ebf3b
Iapetus-11 <olimdahacker@gmail.com>
2020-12-04 11:28:59 -0500

    update pause_econ clear loop

--- cogs/other/mobs.py
+++ cogs/other/mobs.py
@@ -23,10 +23,10 @@ class Mobs(commands.Cog):  # fuck I really don't want to work on this
         self.spawn_events.cancel()
         self.clear_pauses.cancel()
 
-    @tasks.loop(seconds=30)
+    @tasks.loop(seconds=1)
     async def clear_pauses(self):
         for uid in list(self.d.pause_econ):
-            if (arrow.utcnow() - self.d.pause_econ[uid]).seconds > 30:
+            if (arrow.utcnow() - self.d.pause_econ[uid]).seconds > 15:
                 self.d.pause_econ.pop(uid, None)
 
     def engage_check(self, m, ctx):
