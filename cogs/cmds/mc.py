ad2173bc1e47db4cce987a7a07b55fe1b1733a3b
Iapetus-11 <olimdahacker@gmail.com>
2020-12-12 14:57:16 -0500

    fix numbering of names in name history in /mcp

--- cogs/cmds/mc.py
+++ cogs/cmds/mc.py
@@ -343,7 +343,7 @@ class Minecraft(commands.Cog):
                 time = arrow.Arrow.fromtimestamp(time)
                 time = time.format('MMM D, YYYY', locale=ctx.l.lang) + ', ' + time.humanize(locale=ctx.l.lang)
 
-            name_hist += f'**{i}.** `{name.name}` - {time}\n'
+            name_hist += f'**{len(names)-i}.** `{name.name}` - {time}\n'
 
         embed = discord.Embed(color=self.d.cc, title=f'Minecraft profile for `{profile.name}`')
 
