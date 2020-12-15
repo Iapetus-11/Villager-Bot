e0593370a54727b7a8c35d9e74189216bd34ae79
Iapetus-11 <olimdahacker@gmail.com>
2020-12-15 13:28:47 -0500

    code organization changes in mc.py

--- cogs/cmds/mc.py
+++ cogs/cmds/mc.py
@@ -21,12 +21,12 @@ class Minecraft(commands.Cog):
 
         self.bot = bot
 
-        self.d = self.bot.d
-        self.k = self.bot.k
+        self.d = bot.d
+        self.k = bot.k
 
-        self.db = self.bot.get_cog('Database')
+        self.db = bot.get_cog('Database')
 
-        self.ses = aiohttp.ClientSession(loop=self.bot.loop)
+        self.ses = aiohttp.ClientSession(loop=bot.loop)
 
         self.d.mcserver_list = []
 
