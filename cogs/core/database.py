1d601cf2987cbe0433e69bf7b27882543bdf4008
Iapetus-11 <olimdahacker@gmail.com>
2020-12-18 09:36:26 -0500

    make mass_fetch_votestreaks not fetch expired streaks

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -1,5 +1,6 @@
 from discord.ext import commands, tasks
 import discord
+import arrow
 
 
 class Database(commands.Cog):
@@ -161,7 +162,10 @@ class Database(commands.Cog):
 
     async def mass_fetch_votestreaks(self):
         async with self.db.acquire() as con:
-            return await con.fetch('SELECT uid, vote_streak FROM users WHERE vote_streak > 0')
+            return await con.fetch(
+                'SELECT uid, vote_streak FROM users WHERE vote_streak > 0 AND streak_time < $1',
+                arrow.utcnow().shift(days=-1, minutes=-10).timestamp
+            )
 
     async def set_balance(self, uid, emeralds):
         await self.fetch_user(uid)
