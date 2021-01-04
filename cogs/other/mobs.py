b5989cd38926a182cc901ebd77cc969d5fddefda
Iapetus-11 <iapetus011@gmail.com>
2021-01-04 13:39:03 -0500

    bump clear pause time to 15 from 10

--- cogs/other/mobs.py
+++ cogs/other/mobs.py
@@ -27,7 +27,7 @@ class Mobs(commands.Cog):  # fuck I really don't want to work on this
     @tasks.loop(seconds=.75)
     async def clear_pauses(self):
         for uid in list(self.d.pause_econ):
-            if (arrow.utcnow() - self.d.pause_econ[uid]).seconds > 10:
+            if (arrow.utcnow() - self.d.pause_econ[uid]).seconds > 15:
                 self.d.pause_econ.pop(uid, None)
 
     def engage_check(self, m, ctx):
