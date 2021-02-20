c681258bbaaf491ba96d7a4058d5593470071453
Milo Weinberg <iapetus011@gmail.com>
2021-02-19 19:23:35 -0500

    reduce shard count

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ bot = commands.AutoShardedBot(  # setup bot
     case_insensitive=True,
     intents=intents,
     help_command=None,
-    shard_count=21,
+    shard_count=20,
 )
 
 bot.slashbot = SlashCommand(bot, override_type=True)
