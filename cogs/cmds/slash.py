963ccfb0b4f2d67977cb7c59e8c69bc601aae167
Milo Weinberg <iapetus011@gmail.com>
2021-02-20 19:17:50 -0500

    bruh

--- cogs/cmds/slash.py
+++ cogs/cmds/slash.py
@@ -10,38 +10,36 @@ class Slash(commands.Cog):
 
         self.useful = bot.get_cog("Useful")
 
-        self.guild_ids = [641117791272960031]
-
     async def prep(self, ctx):
         ctx.l = await self.bot.get_lang(ctx)
         await ctx.respond()
 
-    @cog_ext.cog_slash(name="help", description="View helpful information about Villager Bot!", guild_ids=self.guild_ids)
+    @cog_ext.cog_slash(name="help", description="View helpful information about Villager Bot!", guild_ids=[641117791272960031])
     async def help(self, ctx):
         await self.prep(ctx)
         await self.useful.help(ctx)
 
-    @cog_ext.cog_slash(base="help", name="econ", description="View helpful information about Villager Bot's economy commands!", guild_ids=self.guild_ids)
+    @cog_ext.cog_slash(base="help", name="econ", description="View helpful information about Villager Bot's economy commands!", guild_ids=[641117791272960031])
     async def help_economy(self, ctx):
         await self.prep(ctx)
         await self.useful.help_economy(ctx)
 
-    @cog_ext.cog_slash(base="help", name="mc", description="View helpful information about Villager Bot's minecraft-related commands!", guild_ids=self.guild_ids)
+    @cog_ext.cog_slash(base="help", name="mc", description="View helpful information about Villager Bot's minecraft-related commands!", guild_ids=[641117791272960031])
     async def help_minecraft(self, ctx):
         await self.prep(ctx)
         await self.useful.help_minecraft(ctx)
 
-    @cog_ext.cog_slash(base="help", name="util", description="View helpful information about Villager Bot's utility commands!", guild_ids=self.guild_ids)
+    @cog_ext.cog_slash(base="help", name="util", description="View helpful information about Villager Bot's utility commands!", guild_ids=[641117791272960031])
     async def help_utility(self, ctx):
         await self.prep(ctx)
         await self.useful.help_utility(ctx)
 
-    @cog_ext.cog_slash(base="help", name="fun", description="View helpful information about Villager Bot's fun & memey commands!", guild_ids=self.guild_ids)
+    @cog_ext.cog_slash(base="help", name="fun", description="View helpful information about Villager Bot's fun & memey commands!", guild_ids=[641117791272960031])
     async def help_fun(self, ctx):
         await self.prep(ctx)
         await self.useful.help_fun(ctx)
 
-    @cog_ext.cog_slash(base="help", name="admin", description="View helpful information about Villager Bot's moderation commands!", guild_ids=self.guild_ids)
+    @cog_ext.cog_slash(base="help", name="admin", description="View helpful information about Villager Bot's moderation commands!", guild_ids=[641117791272960031])
     async def help_administrative(self, ctx):
         await self.prep(ctx)
         await self.useful.help_administrative(ctx)
