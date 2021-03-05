bd498f629ad719abf044eca5c81b12ed06818b57
Milo Weinberg <iapetus011@gmail.com>
2021-03-05 08:16:19 -0500

    update formatting of setup.sql

--- setup.sql
+++ setup.sql
@@ -1,76 +1,76 @@
 CREATE TABLE IF NOT EXISTS guilds (
-  gid            bigint, -- guild id
-  prefix         varchar(15), -- prefix in that server
-  replies        bool, -- whether or not the should reply to "emeralds" and "villager bot"
-  difficulty     varchar(15), -- difficulty for mob spawns and other things
-  lang           varchar(10), -- language used leave null for en-us
-  mcserver       varchar(50), -- default mcserver for the /mcstatus command
-  premium        bool
+  gid            BIGINT, -- guild id
+  prefix         VARCHAR(15), -- prefix in that server
+  replies        BOOL, -- whether or not the should reply to "emeralds" and "villager bot"
+  difficulty     VARCHAR(15), -- difficulty for mob spawns and other things
+  lang           VARCHAR(10), -- language used leave null for en-us
+  mcserver       VARCHAR(50), -- default mcserver for the /mcstatus command
+  premium        BOOL
 );
 
 CREATE TABLE IF NOT EXISTS users (
-  uid           bigint, -- user id
-  emeralds      bigint, -- amount of emeralds user has outside of the vault
-  vault_bal     int, -- amount of emerald BLOCKS which are currently in the vault
-  vault_max     int, -- maximum amount of emerald BLOCKS which can be stored in the vault
-  health        int, -- user health, out of 20
-  bot_banned    bool, -- is banned from using the bot
-  vote_streak   int,
-  streak_time   bigint,
-  give_alert    bool -- whether to tell user if someone gave em stuff or not
+  uid           BIGINT, -- user id
+  emeralds      BIGINT, -- amount of emeralds user has outside of the vault
+  vault_bal     INT, -- amount of emerald BLOCKS which are currently in the vault
+  vault_max     INT, -- maximum amount of emerald BLOCKS which can be stored in the vault
+  health        INT, -- user health, out of 20
+  bot_banned    BOOL, -- is banned from using the bot
+  vote_streak   INT,
+  streak_time   BIGINT,
+  give_alert    BOOL -- whether to tell user if someone gave em stuff or not
 );
 
 CREATE TABLE IF NOT EXISTS items (
-  uid          bigint, -- owner of the item
-  name         varchar(250), -- name of the item
-  sell_price   bigint, -- sell price for ONE of that item
-  amount       bigint,  -- amount of the item
-  sticky       bool -- if true, item can't be traded
+  uid          BIGINT, -- owner of the item
+  name         VARCHAR(250), -- name of the item
+  sell_price   BIGINT, -- sell price for ONE of that item
+  amount       BIGINT,  -- amount of the item
+  sticky       BOOL -- if true, item can't be traded
 );
 
 CREATE TABLE IF NOT EXISTS give_logs (
-  item       varchar(250),
-  amount     bigint,
-  ts         bigint,
-  giver_uid  bigint,
-  recvr_uid  bigint
+  item       VARCHAR(250),
+  amount     BIGINT,
+  ts         BIGINT,
+  giver_uid  BIGINT,
+  recvr_uid  BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS leaderboards ( -- there are more leaderboards than this, not all of them are stored here
-  uid          bigint,
-  pillages     bigint, -- number of pillaged emeralds
-  mobs_killed  bigint
+  uid          BIGINT,
+  pillages     BIGINT, -- number of pillaged emeralds
+  mobs_killed  BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS warnings ( -- moderation warnings
-  uid    bigint, -- receiver of the warning
-  gid    bigint, -- guild in which the warning was done
-  mod_id bigint, -- moderator who did the warning
-  reason varchar(250) -- reason for the warning, null for no reason
+  uid    BIGINT, -- receiver of the warning
+  gid    BIGINT, -- guild in which the warning was done
+  mod_id BIGINT, -- moderator who did the warning
+  reason VARCHAR(250) -- reason for the warning, null for no reason
 );
 
 CREATE TABLE IF NOT EXISTS mcservers ( -- used in /randommc command
-  owner_id bigint, -- discord owner id of the server
-  host     varchar(100), -- hostname/ip/address of server
-  link     varchar(250) -- learn more link
+  owner_id BIGINT, -- discord owner id of the server
+  host     VARCHAR(100), -- hostname/ip/address of server
+  link     VARCHAR(250) -- learn more link
 );
 
 CREATE TABLE IF NOT EXISTS disabled (
-  gid bigint,
-  cmd varchar(20)
+  gid BIGINT,
+  cmd VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS user_rcon (
-  uid       bigint,
-  mcserver  varchar(50),
-  rcon_port int,
-  password  varchar(300)
+  uid       BIGINT,
+  mcserver  VARCHAR(50),
+  rcon_port INT,
+  password  VARCHAR(300)
 );
 
 -- CREATE TABLE IF NOT EXISTS mc_servers( -- used for the !!randommc command
---   owner_id bigint, -- discord user id of the owner
---   address  varchar(100), -- address of the server
---   port     int, -- port which the server is on
---   version  varchar(50), -- Java Edition <version> OR Bedrock Edition
---   note     varchar(250) -- optional note
+--   owner_id BIGINT, -- discord user id of the owner
+--   address  VARCHAR(100), -- address of the server
+--   port     INT, -- port which the server is on
+--   version  VARCHAR(50), -- Java Edition <version> OR Bedrock Edition
+--   note     VARCHAR(250) -- optional note
 -- );
