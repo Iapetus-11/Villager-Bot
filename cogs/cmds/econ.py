2a7f8e29e49637ebe67f72e5d30b7422506de029
Milo Weinberg <iapetus011@gmail.com>
2021-03-14 15:15:20 -0400

    fix some logic relating to using honey jars

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -1073,11 +1073,10 @@ class Econ(commands.Cog):
                 await self.db.remove_item(ctx.author.id, 'Honey Jar', 1)
                 db_user = await self.db.fetch_user(ctx.author.id) # updates health
                 await self.bot.send(ctx, ctx.l.econ.use.chug_honey.format('Honey Jar', db_user['health'], self.d.emojis.heart_full))
-                return
-
             else:
                 await self.bot.send(ctx, ctx.l.econ.use.full_health.format(self.d.emojis.heart_full))
-                return
+
+            return
 
         if thing == "present":
             await self.db.remove_item(ctx.author.id, "Present", 1)
