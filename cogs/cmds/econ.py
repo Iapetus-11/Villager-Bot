63f59569d6cc0ecdcaed4ffa744573fbb123bf4d
Milo Weinberg <iapetus011@gmail.com>
2021-03-17 22:24:51 -0400

    black

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -1100,8 +1100,8 @@ class Econ(commands.Cog):
                 else:
                     await self.bot.send(ctx, ctx.l.econ.use.cant_use_up_to.format(max_amount, "Honey Jar"))
             else:
-                await self.db.update_user(ctx.author.id, 'health', db_user['health']+amount)
-                await self.db.remove_item(ctx.author.id, 'Honey Jar', amount)
+                await self.db.update_user(ctx.author.id, "health", db_user["health"] + amount)
+                await self.db.remove_item(ctx.author.id, "Honey Jar", amount)
 
                 new_health = amount + db_user["health"]
                 await self.bot.send(ctx, ctx.l.econ.use.chug_honey.format(amount, new_health, self.d.emojis.heart_full))
