919ec25f687c04f854cb6e519e4bc1b8a3828ea7
Milo Weinberg <iapetus011@gmail.com>
2021-03-20 15:08:17 -0400

    fix indexing

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -1265,7 +1265,7 @@ class Econ(commands.Cog):
             else:
                 user = discord.utils.escape_markdown(user.display_name)
 
-            body += rank_fstr.format(entry[2] + 1, entry[1], user)
+            body += rank_fstr.format(entry[2], entry[1], user)
 
         # add user if user is missing from the leaderboard
         if u_entry[2] > 9:
