bb0061c10be4ddf5f6694ed4bde0ecbb17cf43eb
Milo Weinberg <iapetus011@gmail.com>
2021-03-21 16:02:16 -0400

    update urls

--- cogs/cmds/fun.py
+++ cogs/cmds/fun.py
@@ -141,7 +141,7 @@ class Fun(commands.Cog):
             await ctx.send(embed=embed)
         else:
             embed = discord.Embed(color=self.d.cc)
-            embed.set_image(url=f"https://iapetus11.me/images/cursed_minecraft/{random.choice(self.d.cursed_images)}")
+            embed.set_image(url=f"https://iapetus11.me/static/images/cursed_minecraft/{random.choice(self.d.cursed_images)}")
 
             await ctx.send(embed=embed)
 
