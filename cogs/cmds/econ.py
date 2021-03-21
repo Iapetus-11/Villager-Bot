44bb34f6f8bbaab217c63d295c23a0de42a5c45d
unknown <lironfrid2007@gmail.com>
2021-03-21 10:37:36 +0200

    merge conflicts pain

--- cogs/cmds/econ.py
+++ cogs/cmds/econ.py
@@ -1108,10 +1108,12 @@ class Econ(commands.Cog):
                     return
 
                 else:
-                    amount = max_amount
+                    await self.bot.send(ctx, ctx.l.econ.use.cant_use_up_to.format(max_amount, "Honey Jar"))
 
-            await self.db.update_user(ctx.author.id, 'health', db_user['health']+amount)
-            await self.db.remove_item(ctx.author.id, 'Honey Jar', amount)
+                await self.db.update_user(ctx.author.id, "health", db_user["health"] + amount)
+                await self.db.remove_item(ctx.author.id, "Honey Jar", amount)
+
+
 
             new_health = amount + db_user["health"]
             await self.bot.send(ctx, ctx.l.econ.use.chug_honey.format(amount, new_health, self.d.emojis.heart_full))
