b42a3ac44b4ed9e906bc876c779f1815ff45c449
Milo Weinberg <iapetus011@gmail.com>
2021-03-23 19:07:37 -0400

    remove hardcoded shard count

--- bot.py
+++ bot.py
@@ -57,7 +57,6 @@ if __name__ == "__main__":
         case_insensitive=True,
         intents=intents,
         help_command=None,
-        shard_count=20,
     )
 
     # bot.slashbot = SlashCommand(
