4c3c9579fb429ac100bd254082194eb5b04d12b6
Milo Weinberg <iapetus011@gmail.com>
2021-03-25 22:48:57 -0400

    update fishing prices every 24 hrs

--- cogs/core/loops.py
+++ cogs/core/loops.py
@@ -10,14 +10,20 @@ class Loops(commands.Cog):
         self.d = bot.d
 
         self.change_status.start()
+        self.update_fishing_prices.start()
 
     def cog_unload(self):
         self.change_status.cancel()
+        self.update_fishing_prices.cancel()
 
     @tasks.loop(minutes=45)
     async def change_status(self):
         await self.bot.wait_until_ready()
         await self.bot.change_presence(activity=discord.Game(name=random.choice(self.d.playing_list)))
+
+    @tasks.loop(hours=24)
+    async def update_fishing_prices(self):
+        self.bot.update_fishing_prices()
 
 
 def setup(bot):
