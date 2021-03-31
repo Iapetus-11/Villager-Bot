489f5c35f5d4b24373332b16cc78599004f4f6e0
Milo Weinberg <iapetus011@gmail.com>
2021-03-31 12:21:53 -0400

    update formatting

--- cogs/cmds/owner.py
+++ cogs/cmds/owner.py
@@ -184,7 +184,7 @@ class Owner(commands.Cog):
 
                     if item == "emerald": item = self.d.emojis.emerald
 
-                    body += f"__[{giver}]({entry['giver_uid']})__ :arrow_right: __{entry['amount']}x **{entry['item']}**__ :arrow_right: __[{receiver}]({entry['recvr_uid']})__ {arrow.get(entry['ts']).humanize()}\n"
+                    body += f"__[{giver}]({entry['giver_uid']})__ *gave* __{entry['amount']}x **{item}**__ *to* __[{receiver}]({entry['recvr_uid']})__ *{arrow.get(entry['ts']).humanize()}*\n"
 
                 embed = discord.Embed(color=self.d.cc, description=body)
                 embed.set_author(name=f"Transaction history for {user}", icon_url=user.avatar_url_as())
