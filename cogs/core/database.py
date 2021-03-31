466fdc6f344ef9f634a53f0168a2e5104769e9b1
Milo Weinberg <iapetus011@gmail.com>
2021-03-31 12:15:37 -0400

    fix missing query arg

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -251,7 +251,7 @@ class Database(commands.Cog):
         return await self.db.fetch("SELECT * FROM give_logs WHERE giver_uid = $1 OR recvr_uid = $1 ORDER BY ts DESC LIMIT $2 OFFSET $3", uid, limit, page*limit)
 
     async def fetch_transactions_page_count(self, uid, limit: int = 10) -> int:
-        return await self.db.fetchval("SELECT COUNT(*) FROM give_logs WHERE giver_uid = $1 OR recvr_uid = $1") / limit
+        return await self.db.fetchval("SELECT COUNT(*) FROM give_logs WHERE giver_uid = $1 OR recvr_uid = $1", uid) / limit
 
     async def fetch_pickaxe(self, uid):
         items_names = [item["name"] for item in await self.fetch_items(uid)]
