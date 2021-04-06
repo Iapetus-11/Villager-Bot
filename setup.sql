961fd88578eca4ca8a0fa381aac7cdc46bbc602c
Milo Weinberg <iapetus011@gmail.com>
2021-04-05 21:23:18 -0400

    add commands table

--- setup.sql
+++ setup.sql
@@ -74,3 +74,11 @@ CREATE TABLE IF NOT EXISTS reminders (
   cid BIGINT NOT NULL,
   at BIGINT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS commands (
+  uid BIGINT NOT NULL,
+  cid BIGINT NOT NULL, -- channel id
+  command VARCHAR(50) NOT NULL, -- command name
+  content VARCHAR(200) NOT NULL,  -- message content
+  at BIGINT NOT NULL -- timestamp
+);
