de42974ef6f4b93f010f4c919ae25736f23798d2
Milo Weinberg <iapetus011@gmail.com>
2021-04-12 17:57:10 -0400

    move files

---
+++ src/bot.py
@@ -0,0 +1,229 @@
+from concurrent.futures import ThreadPoolExecutor
+from discord.ext import commands
+import classyjson as cj
+import asyncio
+import asyncpg
+import discord
+import logging
+import random
+import uvloop
+import arrow
+
+from util.setup import villager_bot_intents, setup_logging, setup_database
+
+# send function/method for easy sending of embed messages with small amounts of text
+async def send(_bot, location, message, respond=False, ping=False):
+    embed = discord.Embed(color=_bot.d.cc, description=message)
+
+    try:
+        if respond and hasattr(location, "reply"):
+            try:
+                await location.reply(embed=embed, mention_author=ping)
+                return True
+            except discord.errors.HTTPException:
+                pass
+
+        await location.send(embed=embed)
+
+        return True
+    except discord.Forbidden:
+        return False
+
+
+# get a lang for a given ctx object
+def get_lang(_bot, ctx):
+    if getattr(ctx, "guild", None) is None:
+        return _bot.langs.en
+
+    return _bot.langs[_bot.d.lang_cache.get(ctx.guild.id, "en")]
+
+
+def get_prefix(_bot, ctx):  # get a prefix for a given ctx
+    if getattr(ctx, "guild", None) is None:
+        return _bot.d.default_prefix
+
+    return _bot.d.prefix_cache.get(ctx.guild.id, _bot.d.default_prefix)
+
+
+# update the role of a member in the support server
+async def update_support_member_role(_bot, member):
+    support_guild = _bot.get_guild(_bot.d.support_server_id)
+    role_map_values = list(_bot.d.role_mappings.values())
+    db = _bot.get_cog("Database")
+    roles = []
+
+    for role in member.roles:
+        if role.id not in role_map_values and role.id != _bot.d.support_server_id:
+            roles.append(role)
+
+    pickaxe_role = _bot.d.role_mappings.get(await db.fetch_pickaxe(member.id))
+    if pickaxe_role is not None:
+        roles.append(support_guild.get_role(pickaxe_role))
+
+    if await db.fetch_item(member.id, "Bane Of Pillagers Amulet") is not None:
+        roles.append(support_guild.get_role(_bot.d.role_mappings.get("BOP")))
+
+    if roles != member.roles:
+        try:
+            await member.edit(roles=roles)
+        except Exception:
+            pass
+
+
+def update_fishing_prices(_bot):
+    for fish in _bot.d.fishing.fish.values():
+        fish.current = random.randint(*fish.value)
+
+
+def populate_null_data_values(_bot):
+    _bot.update_fishing_prices()
+
+    fishes = _bot.d.fishing.fish_ids = list(_bot.d.fishing.fish.keys())
+    _bot.d.fishing.fish_weights = [
+        (len(fishes) - fish_data.rarity) ** _bot.d.fishing.exponent for fish_data in _bot.d.fishing.fish.values()
+    ]
+
+
+async def send_tip(ctx):
+    await asyncio.sleep(1)
+    await ctx.send(f"{random.choice(ctx.l.misc.tip_intros)} {random.choice(ctx.l.misc.tips)}")
+
+
+if __name__ == "__main__":
+    # setup uvloop
+    uvloop.install()
+
+    # set up basic logging
+    logger = setup_logging()
+
+    logger.info("loading private keys...")
+    with open("data/keys.json", "r") as k:  # load bot keys
+        keys = cj.load(k)
+
+    bot = commands.AutoShardedBot(  # setup bot
+        command_prefix=get_prefix,
+        case_insensitive=True,
+        intents=villager_bot_intents(),
+        help_command=None,
+    )
+
+    bot.logger = logger
+
+    bot.send = send.__get__(bot)
+    bot.get_lang = get_lang.__get__(bot)
+    bot.update_support_member_role = update_support_member_role.__get__(bot)
+    bot.update_fishing_prices = update_fishing_prices.__get__(bot)
+    bot.populate_null_data_values = populate_null_data_values.__get__(bot)
+
+    logger.info("setting up connection to database and db pool...")
+    asyncio.get_event_loop().run_until_complete(setup_database(bot, keys))
+
+    logger.info("loading villager bot text from data/text.json...")
+    with open("data/text.json", "r", encoding="utf8") as l:
+        bot.langs = cj.load(l)  # turns it into dot accessible dicts for ez access ~~nice dict bro~~
+
+    logger.info("loading villager bot constant data from data/data.json...")
+    with open("data/data.json", "r", encoding="utf8") as d:
+        bot.d = cj.load(d)  # cj automatically turns json into sets of nested classes and attributes for easy access
+
+    bot.d.cc = discord.Color.green()  # embed color
+
+    bot.k = keys
+    bot.k.fernet = bot.k.fernet.encode("utf-8")
+
+    bot.d.votes_topgg = 0
+    bot.d.cmd_count = 0
+    bot.d.msg_count = 0
+    bot.d.start_time = arrow.utcnow()
+
+    bot.d.miners = {}  # {user_id: commands}
+    bot.d.honey_buckets = None  # list of cooldowns for honey command (econ cog)
+    bot.d.mining.pickaxes = list(bot.d.mining.yields_pickaxes)[::-1]  # get list of pickaxe types from best to worst
+    bot.d.pillagers = {}  # {user_id: pillages}
+    bot.d.pillages = {}  # {user_id: times_pillaged}
+    bot.d.chuggers = {}  # {user_id: [potion, potion]}
+    bot.d.cmd_lb = {}  # {user_id: command_count}
+
+    bot.d.pause_econ = {}  # {uid: starttime}
+    bot.d.spawn_queue = {}  # {ctx: starttime}
+
+    bot.d.rcon_cache = {}  # {uid: rcon_client}
+
+    bot.d.disabled_cmds = {}  # {gid: [disabled cmds]}
+
+    bot.d.ban_cache = []  # [uid, uid,..]
+    bot.d.prefix_cache = {}  # {gid: 'prefix'}
+    bot.d.lang_cache = {}  # {gid: 'lang'}
+
+    bot.d.additional_mcservers = []
+    bot.d.mcserver_list = []
+
+    bot.d.fun_langs.unenchant = {v: k for k, v in bot.d.fun_langs.enchant.items()}  # reverse dict to create unenchantment lang
+
+    bot.owner_locked = False
+
+    bot.cog_list = [  # list of cogs which are to be loaded in the bot
+        "cogs.core.database",
+        "cogs.core.events",
+        "cogs.core.loops",
+        "cogs.cmds.useful",
+        "cogs.cmds.owner",
+        "cogs.cmds.mc",
+        "cogs.cmds.mod",
+        "cogs.cmds.fun",
+        "cogs.cmds.econ",
+        "cogs.cmds.config",
+        "cogs.other.mobs",
+        "cogs.other.statcord",
+        "cogs.other.webhooks",
+    ]
+
+    for cog in bot.cog_list:  # load every cog in bot.cog_list
+        logger.info(f"loading extension: {cog}")
+        bot.load_extension(cog)
+
+    bot.populate_null_data_values()
+
+    @bot.check  # everythingggg goes through here
+    async def global_check(ctx):
+        ctx.l = bot.get_lang(ctx)
+
+        # if bot is locked down to only accept commands from owner
+        if bot.owner_locked and ctx.author.id != 536986067140608041:
+            ctx.custom_err = "ignore"
+        elif ctx.author.id in bot.d.ban_cache:  # if command author is bot banned
+            ctx.custom_err = "bot_banned"
+        elif not bot.is_ready():  # if bot hasn't completely started up yet
+            ctx.custom_err = "not_ready"
+        elif ctx.guild is not None and ctx.command.name in bot.d.disabled_cmds.get(
+            ctx.guild.id, tuple()
+        ):  # if command is disabled
+            ctx.custom_err = "disabled"
+
+        if hasattr(ctx, "custom_err"):
+            return False
+
+        # update the leaderboard + session command count
+        try:
+            bot.d.cmd_lb[ctx.author.id] += 1
+        except KeyError:
+            bot.d.cmd_lb[ctx.author.id] = 1
+
+        bot.d.cmd_count += 1
+
+        if ctx.command.cog and ctx.command.cog.__cog_name__ == "Econ":  # make sure it's an econ command
+            if bot.d.pause_econ.get(ctx.author.id) is not None:
+                ctx.custom_err = "econ_paused"
+                return False
+
+            if random.randint(1, bot.d.mob_chance) == 1:  # spawn mob
+                if ctx.command._buckets._cooldown is not None and ctx.command._buckets._cooldown.per >= 2:
+                    bot.d.spawn_queue[ctx] = arrow.utcnow()
+
+        if random.randint(1, bot.d.tip_chance) == 1:
+            bot.loop.create_task(send_tip(ctx))
+
+        return True
+
+    with ThreadPoolExecutor() as bot.tpool:
+        bot.run(keys.discord)  # run the bot, this is a blocking call
---
+++ src/cogs/core/events.py
@@ -0,0 +1,231 @@
+from discord.ext import commands
+import traceback
+import discord
+import asyncio
+import random
+
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.d = bot.d
+
+        self.db = bot.get_cog("Database")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.bot.logger.info(f"\u001b[36;1mCONNECTED\u001b[0m [{self.bot.shard_count} Shards] [{len(self.bot.cogs)} Cogs]")
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        await asyncio.sleep(1)
+
+        for channel in guild.text_channels:
+            if "general" in channel.name:
+                embed = discord.Embed(
+                    color=self.d.cc,
+                    description=f"Hey y'all! Type `{self.d.default_prefix}help` to get started with Villager Bot!\n"
+                    f"If you need any more help, check out the **[Support Server]({self.d.support})**!",
+                )
+
+                embed.set_author(name="Villager Bot", icon_url=self.d.splash_logo)
+                embed.set_footer(text=f"Made by Iapetus11  |  {self.d.default_prefix}rules for the rules!")
+
+                await channel.send(embed=embed)
+                break
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        await self.db.drop_guild(guild.id)
+
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+        await self.db.clear_warns(user.id, guild.id)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await self.bot.wait_until_ready()
+
+        if member.guild.id == self.d.support_server_id:
+            await self.bot.update_support_member_role(member)
+
+    @commands.Cog.listener()
+    async def on_message(self, m):
+        if m.author.bot:
+            return
+
+        self.d.msg_count += 1
+
+        try:
+            if m.guild is not None and m.guild.id == self.d.support_server_id:
+                if m.type in (
+                    discord.MessageType.premium_guild_subscription,
+                    discord.MessageType.premium_guild_tier_1,
+                    discord.MessageType.premium_guild_tier_2,
+                    discord.MessageType.premium_guild_tier_3,
+                ):
+                    await self.db.add_item(m.author.id, "Barrel", 1024, 1)
+                    await self.bot.send(m.author, f"Thanks for boosting the support server! You've received 1x **Barrel**!")
+                    return
+
+            if m.content.startswith(f"<@!{self.bot.user.id}>"):
+                prefix = self.d.default_prefix
+
+                if m.guild is not None:
+                    prefix = self.d.prefix_cache.get(m.guild.id, self.d.default_prefix)
+
+                lang = self.bot.get_lang(m)
+
+                embed = discord.Embed(color=self.d.cc, description=lang.misc.pingpong.format(prefix, self.d.support))
+
+                embed.set_author(name="Villager Bot", icon_url=self.d.splash_logo)
+                embed.set_footer(text=lang.misc.petus)
+
+                await m.channel.send(embed=embed)
+                return
+
+            if m.guild is not None:
+                content_lowered = m.content.lower()
+
+                if "@someone" in content_lowered:
+                    someones = [
+                        u
+                        for u in m.guild.members
+                        if (
+                            not u.bot
+                            and u.status == discord.Status.online
+                            and m.author.id != u.id
+                            and u.permissions_in(m.channel).read_messages
+                        )
+                    ]
+
+                    if len(someones) > 0:
+                        invis = ("||||\u200B" * 200)[2:-3]
+                        await m.channel.send(f"@someone {invis} {random.choice(someones).mention} {m.author.mention}")
+                else:
+                    if not m.content.startswith(self.d.prefix_cache.get(m.guild.id, self.d.default_prefix)):
+                        if "emerald" in content_lowered:
+                            if (await self.db.fetch_guild(m.guild.id))["replies"]:
+                                await m.channel.send(random.choice(self.d.hmms))
+                        elif "creeper" in content_lowered:
+                            if (await self.db.fetch_guild(m.guild.id))["replies"]:
+                                await m.channel.send("awww{} man".format(random.randint(1, 5) * "w"))
+                        elif "reee" in content_lowered:
+                            if (await self.db.fetch_guild(m.guild.id))["replies"]:
+                                await m.channel.send(random.choice(self.d.emojis.reees))
+                        elif "amogus" in content_lowered:
+                            if (await self.db.fetch_guild(m.guild.id))["replies"]:
+                                await m.channel.send(self.d.emojis.amogus)
+        except discord.errors.Forbidden:
+            pass
+
+    async def debug_error(self, ctx, e, loc=None):
+        self.bot.get_cog("StatCord").error_count += 1
+
+        if loc is None:
+            loc = self.bot.get_channel(self.d.error_channel_id)
+
+        try:
+            ctx.message.content
+        except AttributeError:
+            ctx.message.content = None
+
+        traceback_text = "".join(traceback.format_exception(type(e), e, e.__traceback__, 4))
+        final = f"{ctx.author} (lang={getattr(ctx, 'l', {}).get('lang')}): {ctx.message.content}\n\n{traceback_text}".replace(
+            "``", "\`\`\`"
+        )
+
+        await self.bot.send(loc, f"```py\n{final[:1023 - 6]}```")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, e):
+        try:
+            if isinstance(e, commands.CommandOnCooldown):
+                if ctx.command.name == "mine":
+                    if await self.db.fetch_item(ctx.author.id, "Efficiency I Book") is not None:
+                        e.retry_after -= 0.5
+
+                    if "haste ii potion" in self.d.chuggers.get(ctx.author.id, []):
+                        e.retry_after -= 1
+                    elif "haste i potion" in self.d.chuggers.get(ctx.author.id, []):
+                        e.retry_after -= 0.5
+
+                seconds = round(e.retry_after, 2)
+
+                if seconds <= 0.05:
+                    await ctx.reinvoke()
+                    return
+
+                hours = int(seconds / 3600)
+                minutes = int(seconds / 60) % 60
+                seconds -= round((hours * 60 * 60) + (minutes * 60), 2)
+
+                time = ""
+
+                if hours == 1:
+                    time += f"{hours} {ctx.l.misc.time.hour}, "
+                elif hours > 0:
+                    time += f"{hours} {ctx.l.misc.time.hours}, "
+
+                if minutes == 1:
+                    time += f"{minutes} {ctx.l.misc.time.minute}, "
+                elif minutes > 0:
+                    time += f"{minutes} {ctx.l.misc.time.minutes}, "
+
+                if seconds == 1:
+                    time += f"{round(seconds, 2)} {ctx.l.misc.time.second}"
+                elif seconds > 0:
+                    time += f"{round(seconds, 2)} {ctx.l.misc.time.seconds}"
+
+                await self.bot.send(ctx, random.choice(ctx.l.misc.cooldown_msgs).format(time))
+            elif isinstance(e, commands.NoPrivateMessage):
+                await self.bot.send(ctx, ctx.l.misc.errors.private)
+            elif isinstance(e, commands.MissingPermissions):
+                await self.bot.send(ctx, ctx.l.misc.errors.user_perms)
+            elif isinstance(e, (commands.BotMissingPermissions, discord.errors.Forbidden)):
+                await self.bot.send(ctx, ctx.l.misc.errors.bot_perms)
+            elif getattr(e, "original", None) is not None and isinstance(e.original, discord.errors.Forbidden):
+                await self.bot.send(ctx, ctx.l.misc.errors.bot_perms)
+            elif isinstance(e, commands.MaxConcurrencyReached):
+                # await self.bot.send(ctx, ctx.l.misc.errors.concurrency)
+                await self.bot.send(ctx, ctx.l.misc.errors.nrn_buddy)
+            elif isinstance(e, commands.MissingRequiredArgument):
+                await self.bot.send(ctx, ctx.l.misc.errors.missing_arg)
+            elif isinstance(
+                e,
+                (
+                    commands.BadArgument,
+                    commands.errors.UnexpectedQuoteError,
+                    commands.errors.ExpectedClosingQuoteError,
+                    commands.errors.BadUnionArgument,
+                ),
+            ):
+                await self.bot.send(ctx, ctx.l.misc.errors.bad_arg)
+            elif getattr(ctx, "custom_err", None) == "not_ready":
+                await self.bot.send(ctx, ctx.l.misc.errors.not_ready)
+            elif getattr(ctx, "custom_err", None) == "bot_banned":
+                pass
+            elif getattr(ctx, "custom_err", None) == "econ_paused":
+                await self.bot.send(ctx, ctx.l.misc.errors.nrn_buddy)
+            elif getattr(ctx, "custom_err", None) == "disabled":
+                await self.bot.send(ctx, ctx.l.misc.errors.disabled)
+            elif getattr(ctx, "custom_err", None) == "ignore":
+                return
+            else:
+                # errors to ignore
+                for e_type in (commands.CommandNotFound, commands.NotOwner):
+                    if isinstance(e, e_type) or isinstance(getattr(e, "original", None), e_type):
+                        return
+
+                await self.bot.send(ctx, ctx.l.misc.errors.andioop.format(self.d.support))
+                await self.debug_error(ctx, e)
+        except discord.errors.Forbidden:
+            pass
+        except Exception as e:
+            if not isinstance(getattr(e, "original", None), discord.errors.Forbidden):
+                await self.debug_error(ctx, e)
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
