a3cdcee1d3aff678c0f45d1835c0e5705e1f5b26
Milo Weinberg <iapetus011@gmail.com>
2021-04-13 18:53:01 -0400

    debug math command

--- src/cogs/cmds/useful.py
+++ src/cogs/cmds/useful.py
@@ -277,6 +277,7 @@ class Useful(commands.Cog):
     async def math(self, ctx, *, problem):
         try:
             resp = await self.bot.aiohttp.get(f"https://api.mathjs.org/v4/?expr={urlquote(problem)}")
+            print(resp)
             await self.bot.send(ctx, f"```{await resp.json()}```")
         except Exception:
             await self.bot.send(ctx, ctx.l.useful.meth.oops)
