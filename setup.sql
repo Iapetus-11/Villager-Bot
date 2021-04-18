e1189cb3ef40736d623dd549ead40ea394dee8c6
Milo Weinberg <iapetus011@gmail.com>
2021-04-17 23:25:23 -0400

    add comments

--- setup.sql
+++ setup.sql
@@ -1,3 +1,4 @@
+-- stores guild configuration
 CREATE TABLE IF NOT EXISTS guilds (
   gid            BIGINT PRIMARY KEY NOT NULL, -- guild id
   prefix         VARCHAR(15) NOT NULL, -- prefix in that server
@@ -8,6 +9,7 @@ CREATE TABLE IF NOT EXISTS guilds (
   premium        BOOL
 );
 
+-- a user's economy data
 CREATE TABLE IF NOT EXISTS users (
   uid           BIGINT PRIMARY KEY NOT NULL, -- user id
   emeralds      BIGINT NOT NULL, -- amount of emeralds user has outside of the vault
@@ -20,6 +22,7 @@ CREATE TABLE IF NOT EXISTS users (
   give_alert    BOOL NOT NULL -- whether to tell user if someone gave em stuff or not
 );
 
+-- tracks user's items
 CREATE TABLE IF NOT EXISTS items (
   uid          BIGINT NOT NULL, -- owner of the item
   name         VARCHAR(250) NOT NULL, -- name of the item
@@ -28,6 +31,7 @@ CREATE TABLE IF NOT EXISTS items (
   sticky       BOOL NOT NULL -- if true, item can't be traded
 );
 
+-- tracks economy transactions between users
 CREATE TABLE IF NOT EXISTS give_logs (
   item       VARCHAR(250) NOT NULL,
   amount     BIGINT NOT NULL,
@@ -36,6 +40,7 @@ CREATE TABLE IF NOT EXISTS give_logs (
   recvr_uid  BIGINT NOT NULL
 );
 
+-- used for leaderboards as part of the economy suite
 CREATE TABLE IF NOT EXISTS leaderboards ( -- there are more leaderboards than this, not all of them are stored here
   uid          BIGINT PRIMARY KEY NOT NULL,
   pillages     BIGINT NOT NULL, -- number of pillaged emeralds
@@ -43,24 +48,28 @@ CREATE TABLE IF NOT EXISTS leaderboards ( -- there are more leaderboards than th
   fish         BIGINT NOT NULL -- number of fish fished up
 );
 
-CREATE TABLE IF NOT EXISTS warnings ( -- moderation warnings
+-- used for warning system as part of moderation suite
+CREATE TABLE IF NOT EXISTS warnings (
   uid    BIGINT NOT NULL, -- receiver of the warning
   gid    BIGINT NOT NULL, -- guild in which the warning was done
   mod_id BIGINT NOT NULL, -- moderator who did the warning
   reason VARCHAR(250) -- reason for the warning, null for no reason
 );
 
-CREATE TABLE IF NOT EXISTS mcservers ( -- used in /randommc command
+-- used in !!randommc command
+CREATE TABLE IF NOT EXISTS mcservers (
   owner_id BIGINT NOT NULL, -- discord owner id of the server
   host     VARCHAR(100), -- hostname/ip/address of server
   link     VARCHAR(250) -- learn more link
 );
 
+-- used to store disabled commands for premium servers
 CREATE TABLE IF NOT EXISTS disabled (
   gid BIGINT NOT NULL,
   cmd VARCHAR(20) NOT NULL
 );
 
+-- used to store connection credentials + info for rcon command
 CREATE TABLE IF NOT EXISTS user_rcon (
   uid       BIGINT NOT NULL,
   mcserver  VARCHAR(50),
@@ -68,6 +77,7 @@ CREATE TABLE IF NOT EXISTS user_rcon (
   password  VARCHAR(300)
 );
 
+-- used for reminders command
 CREATE TABLE IF NOT EXISTS reminders (
   uid BIGINT NOT NULL,
   cid BIGINT NOT NULL,
@@ -76,7 +86,8 @@ CREATE TABLE IF NOT EXISTS reminders (
   at BIGINT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user_roles ( -- used to store roles of users in guilds with role persistence on
+-- used to store roles of users in guilds with role persistence on
+CREATE TABLE IF NOT EXISTS user_roles (
   uid BIGINT NOT NULL,
   gid BIGINT NOT NULL,
   roles BIGINT[] NOT NULL DEFAULT {}
