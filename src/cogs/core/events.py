57582f91403cf3a813fa457e7454427e32f1666f
Milo Weinberg <iapetus011@gmail.com>
2021-04-21 11:47:29 -0400

    cythonize error handling

--- src/cogs/core/events.py
+++ src/cogs/core/events.py
@@ -4,7 +4,7 @@ import discord
 import asyncio
 import random
 
-from util.handlers import handle_message
+from util.handlers import handle_message, handle_error
 
 
 class Events(commands.Cog):
@@ -56,7 +56,7 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, m):
         replies = False
-        
+
         try:
             replies = (await self.db.fetch_guild(m.guild.id))["replies"]
         except AttributeError:
@@ -88,88 +88,10 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, e):
         try:
-            if isinstance(e, commands.CommandOnCooldown):
-                if ctx.command.name == "mine":
-                    if await self.db.fetch_item(ctx.author.id, "Efficiency I Book") is not None:
-                        e.retry_after -= 0.5
-
-                    if "haste ii potion" in self.d.chuggers.get(ctx.author.id, []):
-                        e.retry_after -= 1
-                    elif "haste i potion" in self.d.chuggers.get(ctx.author.id, []):
-                        e.retry_after -= 0.5
-
-                seconds = round(e.retry_after, 2)
-
-                if seconds <= 0.05:
-                    await ctx.reinvoke()
-                    return
-
-                hours = int(seconds / 3600)
-                minutes = int(seconds / 60) % 60
-                seconds -= round((hours * 60 * 60) + (minutes * 60), 2)
-
-                time = ""
-
-                if hours == 1:
-                    time += f"{hours} {ctx.l.misc.time.hour}, "
-                elif hours > 0:
-                    time += f"{hours} {ctx.l.misc.time.hours}, "
-
-                if minutes == 1:
-                    time += f"{minutes} {ctx.l.misc.time.minute}, "
-                elif minutes > 0:
-                    time += f"{minutes} {ctx.l.misc.time.minutes}, "
-
-                if seconds == 1:
-                    time += f"{round(seconds, 2)} {ctx.l.misc.time.second}"
-                elif seconds > 0:
-                    time += f"{round(seconds, 2)} {ctx.l.misc.time.seconds}"
-
-                await self.bot.send(ctx, random.choice(ctx.l.misc.cooldown_msgs).format(time))
-            elif isinstance(e, commands.NoPrivateMessage):
-                await self.bot.send(ctx, ctx.l.misc.errors.private)
-            elif isinstance(e, commands.MissingPermissions):
-                await self.bot.send(ctx, ctx.l.misc.errors.user_perms)
-            elif isinstance(e, (commands.BotMissingPermissions, discord.errors.Forbidden)):
-                await self.bot.send(ctx, ctx.l.misc.errors.bot_perms)
-            elif getattr(e, "original", None) is not None and isinstance(e.original, discord.errors.Forbidden):
-                await self.bot.send(ctx, ctx.l.misc.errors.bot_perms)
-            elif isinstance(e, commands.MaxConcurrencyReached):
-                # await self.bot.send(ctx, ctx.l.misc.errors.concurrency)
-                await self.bot.send(ctx, ctx.l.misc.errors.nrn_buddy)
-            elif isinstance(e, commands.MissingRequiredArgument):
-                await self.bot.send(ctx, ctx.l.misc.errors.missing_arg)
-            elif isinstance(
-                e,
-                (
-                    commands.BadArgument,
-                    commands.errors.UnexpectedQuoteError,
-                    commands.errors.ExpectedClosingQuoteError,
-                    commands.errors.BadUnionArgument,
-                ),
-            ):
-                await self.bot.send(ctx, ctx.l.misc.errors.bad_arg)
-            elif getattr(ctx, "custom_err", None) == "not_ready":
-                await self.bot.send(ctx, ctx.l.misc.errors.not_ready)
-            elif getattr(ctx, "custom_err", None) == "bot_banned":
-                pass
-            elif getattr(ctx, "custom_err", None) == "econ_paused":
-                await self.bot.send(ctx, ctx.l.misc.errors.nrn_buddy)
-            elif getattr(ctx, "custom_err", None) == "disabled":
-                await self.bot.send(ctx, ctx.l.misc.errors.disabled)
-            elif getattr(ctx, "custom_err", None) == "ignore":
-                return
-            else:
-                # errors to ignore
-                for e_type in (commands.CommandNotFound, commands.NotOwner):
-                    if isinstance(e, e_type) or isinstance(getattr(e, "original", None), e_type):
-                        return
-
-                await self.bot.send(ctx, ctx.l.misc.errors.andioop.format(self.d.support))
-                await self.debug_error(ctx, e)
+            await asyncio.gather(*handle_error(self, ctx, e))
         except discord.errors.Forbidden:
             pass
-        except Exception as e:
+        except BaseException as e:
             if not isinstance(getattr(e, "original", None), discord.errors.Forbidden):
                 await self.debug_error(ctx, e)
 
