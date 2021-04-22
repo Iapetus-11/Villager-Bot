4aa4ef490721985fad9ddb8e7db1db17be735ce9
Milo Weinberg <iapetus011@gmail.com>
2021-04-22 19:04:13 -0400

    start on Message class

--- src/speedups/message.pyx
+++ src/speedups/message.pyx
@@ -3,7 +3,10 @@ import datetime
 import re
 import io
 
-from .errors import InvalidArgument
+from discord.errors import InvalidArgument
+import discord
+
+from .mixins import Hashable
 
 cpdef str convert_emoji_reaction(emoji: object):
     if isinstance(emoji, Reaction):
@@ -21,3 +24,28 @@ cpdef str convert_emoji_reaction(emoji: object):
         return emoji.strip('<>')
 
     raise InvalidArgument(f'emoji argument must be str, Emoji, or Reaction not {emoji.__class__.__name__}.')
+
+
+cdef class Message(Hashable):
+    cdef object _state
+    cdef signed int id
+    cdef signed int webhook_id
+    cdef list reactions
+    cdef list attachments
+    cdef list embeds
+    cdef object application
+    cdef object activity
+    cdef object channel
+    cdef object _edited_timestamp
+    cdef object type
+    cdef bint pinned
+    cdef object flags
+    cdef bint mention_everyone
+    cdef bint tts
+    cdef str content
+    cdef str nonce
+    cdef list stickers
+    cdef object reference
+
+    def __init__(self, *, state, channel, data):
+        self._state = state
