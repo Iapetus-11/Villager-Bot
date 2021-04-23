b691e8cb36dbb6541d32bff91d31bab7d87e86f0
Milo Weinberg <iapetus011@gmail.com>
2021-04-23 09:05:49 -0400

    update speedups __init__

--- src/speedups/__init__.py
+++ src/speedups/__init__.py
@@ -1,9 +1,12 @@
-import discord
-
+import speedups.activity as activity
 import speedups.gateway as gateway
 import speedups.mixins as mixins
 
 
 def install():
-    discord.gateway = gateway
-    discord.mixins = mixins
+    discord_module = sys.modules.get("discord")
+
+    discord_module.gateway = gateway
+    discord_module.mixins = mixins
+
+    discord_module.activity.BaseActivity = activity.BaseActivity
