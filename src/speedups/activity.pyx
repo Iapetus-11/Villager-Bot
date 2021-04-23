be3f85577a2c7b196298de53be44109b120075f0
Milo Weinberg <iapetus011@gmail.com>
2021-04-23 09:04:52 -0400

    make BaseActivity an extension type

--- src/speedups/activity.pyx
+++ src/speedups/activity.pyx
@@ -1,6 +1,6 @@
 import datetime
 
-class BaseActivity:
+cdef class BaseActivity:
     """The base activity that all user-settable activities inherit from.
     A user-settable activity is one that can be used in :meth:`Client.change_presence`.
     The following types currently count as user-settable:
