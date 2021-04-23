80654dc66127128c3dc429a5e37ae10f798a8481
Milo Weinberg <iapetus011@gmail.com>
2021-04-23 15:09:07 -0400

    str() bot's user id

--- src/util/statcord.py
+++ src/util/statcord.py
@@ -66,7 +66,7 @@ class ShitCordClient:
         self.prev_net_usage = total_net_usage
 
         data = {
-            "id": self.bot.user.id,
+            "id": str(self.bot.user.id),
             "key": self.statcord_key,
             "servers": str(len(self.bot.guilds)),  # server count
             "users": str(len(self.bot.users)),  # user count
