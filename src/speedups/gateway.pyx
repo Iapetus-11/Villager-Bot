759158ccc0258b40e6e92fd98df1058430318c8a
Milo Weinberg <iapetus011@gmail.com>
2021-04-23 08:47:23 -0400

    formatting

--- src/speedups/gateway.pyx
+++ src/speedups/gateway.pyx
@@ -410,6 +410,7 @@ cdef class _DiscordWebSocket:
 
     async def resume(self):
         """Sends the RESUME packet."""
+
         payload = {
             'op': self.RESUME,
             'd': {
@@ -430,9 +431,11 @@ cdef class _DiscordWebSocket:
 
             if len(msg) < 4 or msg[-4:] != b'\x00\x00\xff\xff':
                 return
+
             msg = self._zlib.decompress(self._buffer)
             msg = msg.decode('utf-8')
             self._buffer = bytearray()
+
         msg = json.loads(msg)
 
         log.debug('For Shard ID %s: WebSocket Event: %s', self.shard_id, msg)
@@ -441,6 +444,7 @@ cdef class _DiscordWebSocket:
         op = msg.get('op')
         data = msg.get('d')
         seq = msg.get('s')
+
         if seq is not None:
             self.sequence = seq
 
@@ -459,20 +463,24 @@ cdef class _DiscordWebSocket:
             if op == self.HEARTBEAT_ACK:
                 if self._keep_alive:
                     self._keep_alive.ack()
+
                 return
 
             if op == self.HEARTBEAT:
                 if self._keep_alive:
                     beat = self._keep_alive.get_payload()
                     await self.send_as_json(beat)
+
                 return
 
             if op == self.HELLO:
                 interval = data['heartbeat_interval'] / 1000.0
                 self._keep_alive = KeepAliveHandler(ws=self, interval=interval, shard_id=self.shard_id)
+
                 # send a heartbeat immediately
                 await self.send_as_json(self._keep_alive.get_payload())
                 self._keep_alive.start()
+
                 return
 
             if op == self.INVALIDATE_SESSION:
@@ -482,7 +490,9 @@ cdef class _DiscordWebSocket:
 
                 self.sequence = 0
                 self.session_id = 0
+
                 log.info('Shard ID %s session has been invalidated.', self.shard_id)
+
                 await self.close(code=1000)
                 raise ReconnectWebSocket(self.shard_id, resume=False)
 
