7754da93dc41c193110862f860ff077815595ece
Milo Weinberg <iapetus011@gmail.com>
2021-04-23 18:59:57 -0400

    update patch order

--- src/speedups/__init__.py
+++ src/speedups/__init__.py
@@ -13,11 +13,11 @@ import speedups.utils
 def install():
     discord_module = sys.modules.get("discord")
 
-    # these two were fully reimplemented so just patch em on ez
-    discord_module.gateway = speedups.gateway
-    discord_module.mixins = speedups.mixins
-
     for module in (speedups.activity, speedups.message, speedups.utils):
         for thing in module.__all__:
             if hasattr(discord_module, thing):
                 setattr(discord_module, thing, getattr(module, thing))
+
+    # these two were fully reimplemented so just patch em on ez
+    discord_module.gateway = speedups.gateway
+    discord_module.mixins = speedups.mixins
