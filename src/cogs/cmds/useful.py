affb3bc829187fe9de8e8e69e9340d9a4e81a037
Milo Weinberg <iapetus011@gmail.com>
2021-05-05 09:12:47 -0400

    as_completed() returns futures

--- src/cogs/cmds/useful.py
+++ src/cogs/cmds/useful.py
@@ -202,12 +202,13 @@ class Useful(commands.Cog):
                     return r_user == ctx.author and ctx.channel == react.message.channel and msg.id == react.message.id
 
                 # wait for reaction from message author (1 min)
-                for react, r_user in asyncio.as_completed(
+                for fut in asyncio.as_completed(
                     [
                         self.bot.wait_for("reaction_add", check=author_check, timeout=30),
                         self.bot.wait_for("reaction_remove", check=author_check, timeout=30),
                     ]
                 ):
+                    react, r_user = fut.result()
                     break
             except asyncio.TimeoutError:
                 return
