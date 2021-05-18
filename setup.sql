9e64bb788446ea5e65eea8a679039c0df9c1c291
Milo Weinberg <iapetus011@gmail.com>
2021-05-18 17:09:16 -0400

    comments

--- setup.sql
+++ setup.sql
@@ -1,36 +1,43 @@
 CREATE TABLE IF NOT EXISTS guilds (
-  guild_id        BIGINT PRIMARY KEY, -- the discord guild id / snowflake
-  prefix          VARCHAR(15) NOT NULL DEFAULT '!!', -- the prefix of the guild
-  difficulty      SMALLINT NOT NULL DEFAULT 1, -- the difficulty the server is on 0=peaceful, 1=normal, 2=hard
-  lang            VARCHAR(6) NOT NULL DEFAULT 'en', -- the language the bot will speak in
-  mc_server       VARCHAR(100), -- the minecraft server of the guild
-  premium         BOOLEAN NOT NULL DEFAULT false, -- whether the server is premium or not
-  roles_persist   BOOLEAN NOT NULL DEFAULT false -- whether roles should persist or not
+  guild_id           BIGINT PRIMARY KEY, -- the discord guild id / snowflake
+  prefix             VARCHAR(15) NOT NULL DEFAULT '!!', -- the prefix of the guild
+  difficulty         SMALLINT NOT NULL DEFAULT 1, -- the difficulty the server is on 0=peaceful, 1=normal, 2=hard
+  lang               VARCHAR(6) NOT NULL DEFAULT 'en', -- the language the bot will speak in
+  mc_server          VARCHAR(100), -- the minecraft server of the guild
+  premium            BOOLEAN NOT NULL DEFAULT false, -- whether the server is premium or not
+  roles_persist      BOOLEAN NOT NULL DEFAULT false -- whether roles should persist or not
 );
 
 CREATE TABLE IF NOT EXISTS warnings (
-  user_id         BIGINT PRIMARY KEY,  -- the discord user id / snowflake
-  guild_id        BIGINT NOT NULL, -- the guild where the user was warned
-  mod_id          BIGINT NOT NULL, -- the mod / admin who issued the warning
-  reason          VARCHAR(250) -- the reason for the warning (optional)
+  user_id            BIGINT PRIMARY KEY,  -- the discord user id / snowflake
+  guild_id           BIGINT NOT NULL, -- the guild where the user was warned
+  mod_id             BIGINT NOT NULL, -- the mod / admin who issued the warning
+  reason             VARCHAR(250) -- the reason for the warning (optional)
 );
 
 CREATE TABLE IF NOT EXISTS users (
-  user_id         BIGINT PRIMARY KEY,  -- the discord user id / snowflake
-  emeralds        BIGINT NOT NULL DEFAULT 0, -- the amount of emeralds the user has
-  vault_balance   INT NOT NULL DEFAULT 0, -- the amount of emerald blocks in their vault
-  vault_max       INT NOT NULL DEFAULT 1, -- the maximum amount of emerald blocks in their vault
-  health          SMALLINT NOT NULL DEFAULT 20, -- the amount of health the user currently has
-  banned          BOOLEAN NOT NULL DEFAULT false, -- whether the user has been bot-banned or not
-  vote_streak     INT NOT NULL DEFAULT 0, -- the current vote streak of the user
-  last_vote       TIMESTAMPTZ, -- the time at which the last user voted
-  give_alert      BOOLEAN NOT NULL DEFAULT true -- whether users should be alerted if someone gives them items or emeralds or not
+  user_id            BIGINT PRIMARY KEY,  -- the discord user id / snowflake
+  emeralds           BIGINT NOT NULL DEFAULT 0, -- the amount of emeralds the user has
+  vault_balance      INT NOT NULL DEFAULT 0, -- the amount of emerald blocks in their vault
+  vault_max          INT NOT NULL DEFAULT 1, -- the maximum amount of emerald blocks in their vault
+  health             SMALLINT NOT NULL DEFAULT 20, -- the amount of health the user currently has
+  banned             BOOLEAN NOT NULL DEFAULT false, -- whether the user has been bot-banned or not
+  vote_streak        INT NOT NULL DEFAULT 0, -- the current vote streak of the user
+  last_vote          TIMESTAMPTZ, -- the time at which the last user voted
+  give_alert         BOOLEAN NOT NULL DEFAULT true -- whether users should be alerted if someone gives them items or emeralds or not
 );
 
 CREATE TABLE IF NOT EXISTS items (
-  user_id         BIGINT PRIMARY KEY REFERENCES users (user_id) ON DELETE CASCADE,  -- the discord user id / snowflake
-  name            VARCHAR(50) NOT NULL, -- the name of the item
-  sell_price      INT, -- the price it sells back to the bot for
-  amount          BIGINT NOT NULL, -- the amount of the item the user has
-  sticky          BOOLEAN NOT NULL -- whether the item can be traded / given or not
+  user_id            BIGINT PRIMARY KEY REFERENCES users (user_id) ON DELETE CASCADE, -- the discord user id / snowflake
+  name               VARCHAR(50) NOT NULL, -- the name of the item
+  sell_price         INT, -- the price it sells back to the bot for
+  amount             BIGINT NOT NULL, -- the amount of the item the user has
+  sticky             BOOLEAN NOT NULL -- whether the item can be traded / given or not
+);
+
+CREATE TABLE IF NOT EXISTS leaderboards (
+  user_id            BIGINT PRIMARY KEY REFERENCES users (user_id) ON DELETE CASCADE, -- the discord user id / snowflake
+  pillaged_emeralds  BIGINT NOT NULL DEFAULT 0,
+  mobs_killed        BIGINT NOT NULL DEFAULT 0,
+  fish_fished        BIGINT NOT NULL DEFAULT 0
 );
