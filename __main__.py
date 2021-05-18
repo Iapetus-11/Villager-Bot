dad89a40144e4caa1a65b8348fcbda642597af64
Milo Weinberg <iapetus011@gmail.com>
2021-05-18 14:39:08 -0400

    bye bye

--- __main__.py
+++
@@ -1,9 +0,0 @@
-import pyximport
-import numpy
-
-pyximport.install(language_level=3, reload_support=True, setup_args={"include_dirs": numpy.get_include()})
-
-import src.bot as bot
-
-if __name__ == "__main__":
-    bot.run()
