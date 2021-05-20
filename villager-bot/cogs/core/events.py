db5baaffd726b501f7b4ae827048f331308c2cd4
Milo Weinberg <iapetus011@gmail.com>
2021-05-20 09:53:33 -0400

    test eval packets and response packets

--- villager-bot/cogs/core/events.py
+++ villager-bot/cogs/core/events.py
@@ -11,7 +11,7 @@ class Events(commands.Cog):
         await self.bot.ipc.write_packet({"type": "shard-ready", "shard_id": shard_id})
         self.bot.logger.info(f"Shard {shard_id} \u001b[36;1mREADY\u001b[0m")
 
-        packet_id = await self.bot.ipc.write_packet({"type": "eval", "code": "v.start_time"})
+        packet_id = await self.bot.ipc.write_packet({"type": "eval", "code": "v.start_time"}, expects=True)
         packet = await self.bot.ipc.read_packet(packet_id)
         print(f"shard {shard_id} received {packet}")
 
