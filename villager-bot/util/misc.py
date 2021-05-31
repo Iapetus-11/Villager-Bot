e70a9ea62b78e35a5e5884f0e4c3775daf3b07eb
Milo Weinberg <iapetus011@gmail.com>
2021-05-31 15:46:19 -0400

    add dm_check to util/misc.py

--- villager-bot/util/misc.py
+++ villager-bot/util/misc.py
@@ -1,3 +1,9 @@
 def strip_command(ctx):  # returns message.clean_content excluding the command used
     length = len(ctx.prefix) + len(ctx.invoked_with) + 1
     return ctx.message.clean_content[length:]
+
+def dm_check(ctx):
+    def _dm_check(m):
+        return ctx.author.id == m.author.id and ctx.author.dm_channel.id == m.channel.id
+
+    return _dm_check
