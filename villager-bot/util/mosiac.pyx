4b80bf4974fb23eb27cec367bd391e01389259ce
Milo Weinberg <iapetus011@gmail.com>
2021-06-01 15:58:15 -0400

    rewrite mosaic.pyx

--- villager-bot/util/mosiac.pyx
+++ villager-bot/util/mosiac.pyx
@@ -1,101 +1,110 @@
 import numpy as np
-cimport numpy as np
 import random
 import base64
 import json
 import cv2
 import io
 
+cimport numpy as np
 
-cdef np.ndarray im_from_bytes(b: bytes):
-    return cv2.imdecode(np.frombuffer(b, np.uint8), cv2.IMREAD_COLOR)
-
-
-cdef void draw_image(canvas: np.ndarray, img: np.ndarray, x: int, y: int):
-    canvas[y : y + img.shape[0], x : x + img.shape[1]] = img
-
-cdef dict data
-
-with open("data/block_palette.json", "r") as d:
-    data = json.load(d)
-
-cdef dict palette_bi = dict([(tuple(entry[0]), entry[1]) for entry in data["bi"]])
-cdef dict palette_quad = dict([(tuple(entry[0]), entry[1]) for entry in data["quad"]])
-cdef dict palette_oct = dict([(tuple(entry[0]), entry[1]) for entry in data["oct"]])
-cdef dict palette_map = {k: im_from_bytes(base64.b64decode(v)) for k, v in data["palette"].items()}
-
-cdef signed int xi = data["dims"][0]
-cdef signed int yi = data["dims"][1]
+ctypedef np.uint8_t NPUINT8_t
 
 
-cpdef object generate(source_bytes: bytes, max_dim: double, detailed: bint):
-    cdef np.ndarray source = im_from_bytes(source_bytes)
+cdef class Blockifier:
+    cdef dict palette_bi
+    cdef dict palette_quad
+    cdef dict palette_oct
+    cdef dict palette_map
 
-    cdef double sw = source.shape[1]
-    cdef double sh = source.shape[0]
+    cdef signed int xi
+    cdef signed int yi
 
-    cdef double t = 512
+    def __init__(self):
+        with open("data/block_palette.json", "r") as d:
+            data = json.load(d)
 
-    cdef double ratio
-    cdef double new_w, new_h
+        self.palette_bi = {entry[0]: entry[1] for entry in data["bi"]}
+        self.palette_quad = {entry[0]: entry[1] for entry in data["quad"]}
+        self.palette_oct = {entry[0]: entry[1] for entry in data["oct"]}
+        self.palette_map = {k: self.im_from_bytes(base64.b64decode(v)) for k, v in data["palette"].items()}
 
-    # rescale if too big
-    if sw > max_dim or sh > max_dim or detailed:
-        ratio = sw / sh
+        self.xi = data["dims"][0]
+        self.yi = data["dims"][1]
 
-        if sw > sh:
-            new_w = max_dim
-            new_h = new_w / ratio
-        elif sw == sh:
-            new_w = max_dim
-            new_h = max_dim
-        else:
-            new_h = max_dim
-            new_w = new_h * ratio
+    cdef np.ndarray[NPUINT8_t, ndim=3] im_from_bytes(self, b: bytes):
+        return cv2.imdecode(np.frombuffer(b, np.uint8), cv2.IMREAD_COLOR)
 
-        source = cv2.resize(source, (int(new_w), int(new_h)))
-    elif sw < t or sh < t:
-        ratio = sw / sh
+    cdef void draw_image(self, np.ndarray[NPUINT8_t, ndim=3] canvas, np.ndarray[NPUINT8_t, ndim=3] img, signed int x, signed int y):
+        canvas[y : y + img.shape[0], x : x + img.shape[1]] = img
 
-        if sw > sh:
-            new_w = t
-            new_h = new_w / ratio
-        elif sw == sh:
-            new_w = t
-            new_h = t
-        else:
-            new_h = t
-            new_w = new_h * ratio
+    cpdef object generate(self, source_bytes: bytes, max_dim: double, detailed: bint):
+        cdef np.ndarray[NPUINT8_t, ndim=3] source = self.im_from_bytes(source_bytes)
 
-        source = cv2.resize(source, (int(new_w), int(new_h)))
+        cdef double sw = source.shape[1]
+        cdef double sh = source.shape[0]
 
-    source = cv2.resize(source, (int(source.shape[1] / xi), int(source.shape[0] / yi)))
-    cdef np.ndarray canvas = np.zeros((source.shape[0] * xi, source.shape[1] * yi, 3), np.uint8)
+        cdef double t = 512
 
-    cdef signed int x = 0
-    cdef signed int y = 0
-    cdef np.ndarray row
-    cdef signed int b, g, r
-    cdef str pal_key
+        cdef double ratio
+        cdef double new_w, new_h
 
-    for row in source:
-        x = 0
+        # rescale if too big
+        if sw > max_dim or sh > max_dim or detailed:
+            ratio = sw / sh
 
-        for b, g, r in row:
-            pal_key = palette_oct.get((r // 32, g // 32, b // 32))
+            if sw > sh:
+                new_w = max_dim
+                new_h = new_w / ratio
+            elif sw == sh:
+                new_w = max_dim
+                new_h = max_dim
+            else:
+                new_h = max_dim
+                new_w = new_h * ratio
 
-            if pal_key is None:
-                pal_key = palette_quad.get((r // 64, g // 64, b // 64))
+            source = cv2.resize(source, (int(new_w), int(new_h)))
+        elif sw < t or sh < t:
+            ratio = sw / sh
+
+            if sw > sh:
+                new_w = t
+                new_h = new_w / ratio
+            elif sw == sh:
+                new_w = t
+                new_h = t
+            else:
+                new_h = t
+                new_w = new_h * ratio
+
+            source = cv2.resize(source, (int(new_w), int(new_h)))
+
+        source = cv2.resize(source, (source.shape[1] // self.xi, source.shape[0] // self.yi))
+        cdef np.ndarray[NPUINT8_t, ndim=3] canvas = np.zeros((source.shape[0] * self.xi, source.shape[1] * self.yi, 3), np.uint8)
+
+        cdef signed int x = 0
+        cdef signed int y = 0
+        cdef np.ndarray row
+        cdef signed int b, g, r
+        cdef str pal_key
+
+        for row in source:
+            x = 0
+
+            for b, g, r in row:
+                pal_key = self.palette_oct.get((r // 32, g // 32, b // 32))
 
                 if pal_key is None:
-                    pal_key = palette_bi.get((r // 128, g // 128, b // 128))
+                    pal_key = self.palette_quad.get((r // 64, g // 64, b // 64))
 
                     if pal_key is None:
-                        pal_key = palette_oct[random.choice(tuple(palette_oct.keys()))]
+                        pal_key = self.palette_bi.get((r // 128, g // 128, b // 128))
 
-            draw_image(canvas, palette_map[pal_key], x, y)
+                        if pal_key is None:
+                            pal_key = self.palette_oct[random.choice(tuple(self.palette_oct.keys()))]
 
-            x += xi
-        y += yi
+                self.draw_image(canvas, self.palette_map[pal_key], x, y)
 
-    return io.BytesIO(cv2.imencode(".png", canvas)[1])
+                x += self.xi
+            y += self.yi
+
+        return io.BytesIO(cv2.imencode(".png", canvas)[1])
