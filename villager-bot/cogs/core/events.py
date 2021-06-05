d68afb5da0fe60aea40a6b82beee4c5c4e1858d9
Milo Weinberg <iapetus011@gmail.com>
2021-06-04 20:42:14 -0400

    improve command error logging

--- villager-bot/cogs/core/events.py
+++ villager-bot/cogs/core/events.py
@@ -215,10 +215,7 @@ class Events(commands.Cog):
             except Exception:
                 pass
 
-            debug_info = (
-                f"`{ctx.author}` `{ctx.author.id}` (lang={ctx.l.lang}): ```\n{ctx.message.content[:100]}```"
-                f"```py\n{format_exception(e)}"[: 2000 - 3] + "```"
-            )
+            debug_info = f"```\n{ctx.author} {ctx.author.id} (lang={ctx.l.lang}): {ctx.message.content}```"[:200] + "```" + f"```py\n{format_exception(e)}"[:2000-206] + "```"
 
             error_channel = await self.bot.fetch_error_channel()
             await error_channel.send(debug_info)
