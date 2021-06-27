d2f0fe857238298afaabb0a44e1cf7d365933cb9
Milo Weinberg <iapetus011@gmail.com>
2021-06-27 12:51:21 -0400

    minor optimization

--- villager-bot/bot.py
+++ villager-bot/bot.py
@@ -66,7 +66,7 @@ class VillagerBotShardGroup(commands.AutoShardedBot):
         self.logger = setup_logging(self.shard_ids)
         self.ipc = Client(self.k.manager.host, self.k.manager.port, self.handle_broadcast)  # ipc client
         self.aiohttp = aiohttp.ClientSession()
-        self.statcord = StatcordClusterClient(self, self.k.statcord, ".".join([str(shard_id) for shard_id in shard_ids]))
+        self.statcord = StatcordClusterClient(self, self.k.statcord, ".".join(map(str, shard_ids)))
         self.db = None  # asyncpg database connection pool
         self.tp = None  # ThreadPoolExecutor instance
 
