a2aeaf95f65246def4e28e1b09114fa03f5d8672
Milo Weinberg <iapetus011@gmail.com>
2021-06-28 16:37:12 -0400

    add Badges cog and helper methods

---
+++ villager-bot/cogs/core/badges.py
@@ -0,0 +1,43 @@
+from discord.ext import commands
+import asyncpg
+
+from util.misc import calc_total_wealth
+
+
+class Badges(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.db = bot.get_cog("Database")
+
+        self.badges = {}  # {user_id: {badge: value}}
+
+    async def update_user_badges(self, user_id, **kwargs):
+        badges = self.badges.get(user_id)
+
+        if badges is None:
+            self.badges[user_id] = badges = await self.db.fetch_user_badges(user_id)
+
+        for badge, value in kwargs.items():
+            badges[badge] = value
+
+        await self.db.update_user_badges(user_id, **kwargs)
+
+    async def update_uncle_scrooge(self, user_id: int, db_user: asyncpg.Record) -> None:
+        badges = self.badges.get(user_id)
+
+        if badges is None:
+            self.badges[user_id] = badges = dict(await self.db.fetch_user_badges(user_id))
+
+        if badges["uncle_scrooge"]:
+            return
+
+        user_items = await self.db.fetch_items(user_id)
+        total_wealth = calc_total_wealth(db_user, user_items)
+
+        if total_wealth > 100_000:
+            await self.update_user_badges(user_id, uncle_scrooge=True)
+
+
+def setup(bot):
+    bot.add_cog(Badges(bot))
