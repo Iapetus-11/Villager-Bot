bc609512bc64c4acb9d52ca2474bc0652b4b43a4
Milo Weinberg <iapetus011@gmail.com>
2021-07-22 18:26:51 -0400

    fixes

--- villager-bot/cogs/core/database.py
+++ villager-bot/cogs/core/database.py
@@ -23,9 +23,9 @@ class Database(commands.Cog):
         self.bot.ban_cache = await self.fetch_all_botbans()
 
         # per-guild caches, should only load settings for guilds the process can actually see
+        self.bot.disabled_commands.update(await self.fetch_all_disabled_commands())
         self.bot.language_cache = await self.fetch_all_guild_langs()
         self.bot.prefix_cache = await self.fetch_all_guild_prefixes()
-        self.bot.disabled_commands = await self.fetch_all_disabled_commands()
         self.bot.replies_cache = await self.fetch_all_do_replies()
 
     async def fetch_current_reminders(self) -> List[asyncpg.Record]:
