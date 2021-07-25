4b49e39a614778048c9fbbe9b8eb8f65876556e7
Milo Weinberg <iapetus011@gmail.com>
2021-07-25 15:06:04 -0400

    debug

--- villager-bot/util/misc.py
+++ villager-bot/util/misc.py
@@ -136,9 +136,11 @@ async def update_support_member_role(bot, member):
         if await db.fetch_item(member.id, "Bane Of Pillagers Amulet") is not None:
             roles.append(support_guild.get_role(bot.d.role_mappings.get("BOP")))
 
+        print(roles)
+
         if roles != member.roles:
             await member.edit(roles=roles)
-            
+
             # try:
             #     await member.edit(roles=roles)
             # except (discord.errors.Forbidden, discord.errors.HTTPException):
