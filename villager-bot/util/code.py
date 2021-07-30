623a487dbc873c40de081904f332910cca1c3d0c
Milo Weinberg <iapetus011@gmail.com>
2021-07-30 11:49:02 -0400

    fix execute_code()

--- villager-bot/util/code.py
+++ villager-bot/util/code.py
@@ -7,21 +7,26 @@ def format_exception(e: Exception) -> str:
 
 
 async def execute_code(code: str, env: dict) -> object:
-    code_parsed = ast.parse("async def _eval_code():\n" + "\n".join(f"    {i}" for i in code.splitlines()))
-
     def insert_returns(body):
-        if isinstance(body[-1], ast.Expr):
-            body[-1] = ast.Return(body[-1].value)
-            ast.fix_missing_locations(body[-1])
+        try:
+            if isinstance(body[-1], ast.Expr):
+                body[-1] = ast.Return(body[-1].value)
+                ast.fix_missing_locations(body[-1])
 
-        if isinstance(body[-1], ast.If):
-            insert_returns(body[-1].body)
-            insert_returns(body[-1].orelse)
+            if isinstance(body[-1], ast.If):
+                insert_returns(body[-1].body)
+                insert_returns(body[-1].orelse)
 
-        if isinstance(body[-1], ast.With):
-            insert_returns(body[-1].body)
+            if isinstance(body[-1], ast.With):
+                insert_returns(body[-1].body)
+        except IndexError:
+            pass
 
-    insert_returns(code_parsed.body[0].body)
+    func_content = '\n'.join(f'    {line}' for line in code.strip('\n').splitlines())
+    code = f'async def _execute_code():\n{func_content}'
 
-    exec(compile(code_parsed, filename="<ast>", mode="exec"), env)
-    return await eval("_eval_code()", env)
+    parsed = ast.parse(code)
+    insert_returns(parsed.body[0].body)
+
+    exec(compile(parsed, filename="<ast>", mode="exec"), env)
+    return await eval("_execute_code()", env)
