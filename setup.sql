2665e1ef204c8fd44b0a1e6fa49ef0406a4372e6
Milo Weinberg <iapetus011@gmail.com>
2021-08-04 12:53:09 -0400

    sort tables

--- setup.sql
+++ setup.sql
@@ -9,13 +9,6 @@ CREATE TABLE IF NOT EXISTS guilds (
   premium            BOOLEAN NOT NULL DEFAULT false -- whether the server is premium or not
 );
 
-CREATE TABLE IF NOT EXISTS warnings (
-  user_id            BIGINT NOT NULL,  -- the discord user id / snowflake
-  guild_id           BIGINT NOT NULL, -- the guild where the user was warned
-  mod_id             BIGINT NOT NULL, -- the mod / admin who issued the warning
-  reason             VARCHAR(250) -- the reason for the warning (optional)
-);
-
 CREATE TABLE IF NOT EXISTS users ( -- used for economy stuff
   user_id            BIGINT PRIMARY KEY,  -- the discord user id / snowflake
   bot_banned         BOOLEAN NOT NULL DEFAULT false, -- whether the user is botbanned or not
@@ -54,14 +47,6 @@ CREATE TABLE IF NOT EXISTS badges (
   fisherman          SMALLINT NOT NULL DEFAULT 0 -- given for fishing up fishies, levels I-IV
 );
 
-CREATE TABLE IF NOT EXISTS give_logs (
-  item               VARCHAR(250) NOT NULL, -- item traded / given, "emerald" for emeralds
-  amount             BIGINT NOT NULL, -- the amount of the item
-  at                 TIMESTAMPTZ, -- the time at which the transaction was made
-  sender             BIGINT NOT NULL, -- who gave the items in the first place
-  receiver           BIGINT NOT NULL -- who received the items
-);
-
 CREATE TABLE IF NOT EXISTS leaderboards (  -- stores leaderboards which aren't stored elsewhere
   user_id            BIGINT PRIMARY KEY REFERENCES users (user_id) ON DELETE CASCADE, -- the discord user id / snowflake
   pillaged_emeralds  BIGINT NOT NULL DEFAULT 0, -- emeralds pillaged from other users
@@ -70,12 +55,27 @@ CREATE TABLE IF NOT EXISTS leaderboards (  -- stores leaderboards which aren't s
   commands           BIGINT NOT NULL DEFAULT 0 -- not super accurate as commands are cached for speed
 );
 
+CREATE TABLE IF NOT EXISTS give_logs (
+  item               VARCHAR(250) NOT NULL, -- item traded / given, "emerald" for emeralds
+  amount             BIGINT NOT NULL, -- the amount of the item
+  at                 TIMESTAMPTZ, -- the time at which the transaction was made
+  sender             BIGINT NOT NULL, -- who gave the items in the first place
+  receiver           BIGINT NOT NULL -- who received the items
+);
+
 CREATE TABLE IF NOT EXISTS reminders (
   user_id            BIGINT NOT NULL, -- the discord user id / snowflake
   channel_id         BIGINT NOT NULL, -- the channel id where the reminder command was summoned
   message_id         BIGINT NOT NULL, -- the message where the reminder command was summoned
   reminder           TEXT NOT NULL, -- the actual text for the reminder
   at                 TIMESTAMPTZ -- the time at which the user should be reminded
+);
+
+CREATE TABLE IF NOT EXISTS warnings (
+  user_id            BIGINT NOT NULL,  -- the discord user id / snowflake
+  guild_id           BIGINT NOT NULL, -- the guild where the user was warned
+  mod_id             BIGINT NOT NULL, -- the mod / admin who issued the warning
+  reason             VARCHAR(250) -- the reason for the warning (optional)
 );
 
 CREATE TABLE IF NOT EXISTS disabled_commands (
