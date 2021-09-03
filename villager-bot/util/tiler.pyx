3635e138ac7a9688d323d3849b1652f5f1712208
Milo Weinberg <iapetus011@gmail.com>
2021-09-03 11:57:10 -0400

    add convert_video and rename convert_image

--- villager-bot/util/tiler.pyx
+++ villager-bot/util/tiler.pyx
@@ -1,4 +1,5 @@
 import numpy as np
+import imageio
 import random
 import base64
 import json
@@ -19,27 +20,25 @@ cdef class Tiler:
     cdef signed int xi
     cdef signed int yi
 
-    def __init__(self, palette: str):
-        with open(palette, "r") as d:
+    def __init__(self, palette_file: str):
+        with open(palette_file, "r") as d:
             data = json.load(d)
 
         self.palette_bi = {tuple(entry[0]): entry[1] for entry in data["bi"]}
         self.palette_quad = {tuple(entry[0]): entry[1] for entry in data["quad"]}
         self.palette_oct = {tuple(entry[0]): entry[1] for entry in data["oct"]}
-        self.palette_map = {k: self.im_from_bytes(base64.b64decode(v)) for k, v in data["palette"].items()}
+        self.palette_map = {k: self.image_from_bytes(base64.b64decode(v)) for k, v in data["palette"].items()}
 
         self.xi = data["dims"][0]
         self.yi = data["dims"][1]
 
-    cdef np.ndarray[NPUINT8_t, ndim=3] im_from_bytes(self, b: bytes):
+    cdef np.ndarray[NPUINT8_t, ndim=3] image_from_bytes(self, bytes b):
         return cv2.imdecode(np.frombuffer(b, np.uint8), cv2.IMREAD_COLOR)
 
     cdef void draw_image(self, np.ndarray[NPUINT8_t, ndim=3] canvas, np.ndarray[NPUINT8_t, ndim=3] img, signed int x, signed int y):
         canvas[y : y + img.shape[0], x : x + img.shape[1]] = img
 
-    cpdef object generate(self, source_bytes: bytes, max_dim: double, detailed: bint):
-        cdef np.ndarray[NPUINT8_t, ndim=3] source = self.im_from_bytes(source_bytes)
-
+    cdef np.ndarray[NPUINT8_t, ndim=3] prep_image(self, np.ndarray[NPUINT8_t, ndim=3] source, double max_dim, bint detailed):
         cdef double sw = source.shape[1]
         cdef double sh = source.shape[0]
 
@@ -78,7 +77,9 @@ cdef class Tiler:
 
             source = cv2.resize(source, (int(new_w), int(new_h)))
 
-        source = cv2.resize(source, (source.shape[1] // self.xi, source.shape[0] // self.yi))
+        return cv2.resize(source, (source.shape[1] // self.xi, source.shape[0] // self.yi))
+
+    cdef np.ndarray[NPUINT8_t, ndim=3] _convert_image(self, np.ndarray[NPUINT8_t, ndim=3] source):
         cdef np.ndarray[NPUINT8_t, ndim=3] canvas = np.zeros((source.shape[0] * self.xi, source.shape[1] * self.yi, 3), np.uint8)
 
         cdef signed int x = 0
@@ -107,4 +108,35 @@ cdef class Tiler:
                 x += self.xi
             y += self.yi
 
-        return io.BytesIO(cv2.imencode(".png", canvas)[1])
+        return canvas
+
+    cpdef object convert_image(self, bytes source_bytes, double max_dim, bint detailed):
+        cdef np.ndarray[NPUINT8_t, ndim=3] source = self.image_from_bytes(source_bytes)
+        del source_bytes  # saves memory
+        source = self.prep_image(source, max_dim, detailed)
+
+        return io.BytesIO(cv2.imencode(".png", self._convert_image(source))[1])
+
+    cpdef object convert_video(self, bytes source_bytes, double max_dim, bint detailed):
+        cdef object video_capture = cv2.VideoCapture(source_bytes)
+        del source_bytes  # saves memory
+
+        cdef np.ndarray[NPUINT8_t, ndim=4] out_frames = np.zeros((video_capture.get(cv2.CAP_PROP_FRAME_COUNT), video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT) * self.xi, video_capture.get(cv2.CAP_PROP_FRAME_WIDTH) * self.yi, 4), np.uint8)
+
+        cdef bint success
+        cdef np.ndarray[NPUINT8_t, ndim=3] frame
+        cdef int frame_counter = 0
+
+        while True:
+            success, frame = video_capture.read()
+
+            if not success:
+                break
+
+            frame = self.prep_image(frame, max_dim, detailed)
+            self.draw_image(out_frames[frame_counter], self._convert_image(frame), 0, 0)
+            frame_counter += 1
+        
+        cdef object out_bytes_io = io.BytesIO()
+        imageio.mimwrite(out_bytes_io, out_frames, format="gif")
+        return out_bytes_io
