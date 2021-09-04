e7fde0f95c1d6c1879bbb2064932bee1cc7fe231
Milo Weinberg <iapetus011@gmail.com>
2021-09-04 09:05:20 -0400

    attempt to color issues

--- villager-bot/data/blockifier_generator.py
+++ villager-bot/data/blockifier_generator.py
@@ -113,7 +113,7 @@ class Palette:
         avgs[1] /= p_count
         avgs[2] /= p_count
 
-        avgs.reverse()  # turn into rgb
+        # avgs.reverse()  # turn into rgb
 
         b = base64.b64encode(cv2.imencode(".png", img)[1]).decode("utf-8")
 
