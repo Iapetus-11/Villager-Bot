43da2c45806426148f8f52ac42393cedb61e6752
Milo Weinberg <iapetus011@gmail.com>
2021-11-25 10:26:23 -0500

    wtf

--- villager-bot/cogs/commands/owner.py
+++ villager-bot/cogs/commands/owner.py
@@ -23,7 +23,7 @@ class Owner(commands.Cog):
         self.db = bot.get_cog("Database")
 
     @commands.before_invoke
-    async def cog_before_invoke(self, ctx):
+    async def before_cmd_invoke(self, ctx):
         print(f"{ctx.author}: {ctx.message.content}")
 
     @commands.command(name="reload")
