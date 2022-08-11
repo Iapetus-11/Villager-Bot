3894610ebe08c914c8ff3d048f356b9df52268fe
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2022-08-11 16:50:51 +0000

    format code

--- bot/utils/add_cython_ext.py
+++ bot/utils/add_cython_ext.py
@@ -1,8 +1,9 @@
 import numpy
 import pyximport
 
+
 def add_cython_ext():
     # add cython support, with numpy header files
     pyximport.install(language_level=3, setup_args={"include_dirs": numpy.get_include()})
-    
-    import bot.utils.tiler # noqa: F401
+
+    import bot.utils.tiler  # noqa: F401
