4691613993f73ecaeb108237e7dfbb9aa93111b1
Milo Weinberg <iapetus011@gmail.com>
2023-05-17 22:01:50 -0400

    Stop flake8 from complaining

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,3 @@
-from .check_text import run as check_text
-from .format import run as format
-from .generate_blockify_data import run as generate_blockify_data
+from .check_text import run as check_text  # noqa: F401
+from .format import run as format  # noqa: F401
+from .generate_blockify_data import run as generate_blockify_data  # noqa: F401
