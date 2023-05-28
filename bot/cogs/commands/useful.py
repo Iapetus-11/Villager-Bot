1c1b2257f93b36758f63d3aa4374c14ef75e0a96
Infernum1 <81740112+infernum1@noreply.github.com>
2023-05-28 20:49:03 +0530

    minor convenience changes

--- bot/cogs/commands/useful.py
+++ bot/cogs/commands/useful.py
@@ -636,29 +636,31 @@ class Useful(commands.Cog):
         await ctx.reply_embed(ctx.l.useful.search.nope)
         return
 
-    @commands.group(name="reminder", aliases=["remind"])
+    @commands.group(name="reminders", aliases=["reminder", "remind"])
     @commands.cooldown(1, 2, commands.BucketType.user)
-    async def reminder_parent(self, ctx: Ctx):
-        if not ctx.invoked_subcommand:
-            user_reminders = await self.db.fetch_user_reminders(ctx.author.id)
-            embed = discord.Embed(color=self.bot.embed_color)
-            embed.set_author(
-                name=f"{ctx.author.display_name}'s reminders", icon_url=ctx.author.avatar.url
+    async def reminders(self, ctx: Ctx):
+        if ctx.invoked_subcommand:
+            return
+
+        user_reminders = await self.db.fetch_user_reminders(ctx.author.id)
+        embed = discord.Embed(color=self.bot.embed_color)
+        embed.set_author(
+            name=f"{ctx.author.display_name}'s reminders", icon_url=ctx.author.avatar.url
+        )
+
+        for reminder in user_reminders:
+            unix_timestamp = format_dt(reminder["at"], style="R")
+            reminder["reminder"] = shorten_text(reminder["reminder"], 75)
+            embed.add_field(
+                name=f"`#{reminder['id']}` - {unix_timestamp}",
+                value=reminder["reminder"],
+                inline=False,
             )
+        await ctx.send(embed=embed)
 
-            for reminder in user_reminders:
-                unix_timestamp = format_dt(reminder["at"], style="R")
-                reminder["reminder"] = shorten_text(reminder["reminder"], 75)
-                embed.add_field(
-                    name=f"`#{reminder['id']}` - {unix_timestamp}",
-                    value=reminder["reminder"],
-                    inline=False,
-                )
-            await ctx.send(embed=embed)
-
-    @reminder_parent.command(name="add", aliases=["create"])
+    @reminders.command(name="add", aliases=["create"])
     @commands.cooldown(1, 2, commands.BucketType.user)
-    async def reminder_add(self, ctx: Ctx, duration_str: str, *, reminder: str):
+    async def reminders_add(self, ctx: Ctx, duration_str: str, *, reminder: str):
         user_reminder_count = await self.db.fetch_user_reminder_count(ctx.author.id)
 
         if user_reminder_count > 10:
@@ -691,9 +693,9 @@ class Useful(commands.Cog):
             )
         )
 
-    @reminder_parent.command(name="delete", aliases=["remove"])
+    @reminders.command(name="delete", aliases=["remove"])
     @commands.cooldown(1, 2, commands.BucketType.user)
-    async def reminder_remove(self, ctx: Ctx, reminder_id: int):
+    async def reminders_remove(self, ctx: Ctx, reminder_id: int):
         deleted = await self.db.delete_user_reminder(ctx.author.id, reminder_id)
         if deleted:
             await ctx.reply_embed(ctx.l.useful.remind.authorized.format(self.bot.d.emojis.yes))
