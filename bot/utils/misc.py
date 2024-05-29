b35ab17defa3af1af7047716e8b4614e2d805106
Milo Weinberg <iapetus011@gmail.com>
2024-05-28 20:27:11 -0400

    Fix disgusting logic in fetch_aprox_ban_count

--- bot/utils/misc.py
+++ bot/utils/misc.py
@@ -382,8 +382,8 @@ def item_case(text: str) -> str:
 async def fetch_aprox_ban_count(guild: discord.Guild, seconds: float, chunk_size: int = 500) -> str:
     assert chunk_size > 0
 
-    start_time = time.time()
-    last_entry = discord.guild.MISSING
+    start_time: float = time.time()
+    last_entry: discord.User | None = None
     ban_count = 0
 
     while True:
@@ -391,18 +391,14 @@ async def fetch_aprox_ban_count(guild: discord.Guild, seconds: float, chunk_size
         if (time.time() - start_time) > seconds:
             return f"{ban_count}+"
 
-        async for entry in guild.bans(limit=500, after=last_entry):
-            last_entry = entry.user
-            ban_count += 1
-
         ban_entries_chunk = [e async for e in guild.bans(limit=chunk_size, after=last_entry)]
 
         if ban_entries_chunk:
-            last_entry = ban_entries_chunk[-1]
+            last_entry = ban_entries_chunk[-1].user
             ban_count += len(ban_entries_chunk)
 
         # there are no more ban entries to fetch
-        if len(ban_entries_chunk) < chunk_size:
+        if len(ban_entries_chunk) < chunk_size or last_entry is None:
             return str(ban_count)
 
 
