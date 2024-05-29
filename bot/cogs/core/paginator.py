345aafd99d4236aec93833c5ab1a809e0fb38e37
iMaxii <maxithepanda.games@gmail.com>
2024-05-29 15:11:54 -0400

    Replace Reaction-Based Paginator with Button-Based Paginator

--- bot/cogs/core/paginator.py
+++ bot/cogs/core/paginator.py
@@ -1,6 +1,5 @@
-import asyncio
 import inspect
-from typing import Any, Callable, Coroutine
+from typing import Any, Callable, Coroutine, Optional, Union
 
 import discord
 from discord.ext import commands
@@ -8,26 +7,57 @@ from discord.ext import commands
 from bot.utils.ctx import Ctx
 from bot.villager_bot import VillagerBotCluster
 
-PAGE_EMBED_CALLABLE = Callable[[int], discord.Embed | Coroutine[Any, Any, discord.Embed]]
-LEFT_ARROW = "⬅️"
-RIGHT_ARROW = "➡️"
-SKIP_TO_LAST = "⏩"
-BACK_TO_FIRST = "⏪"
-NAV_EMOJIS = [BACK_TO_FIRST, LEFT_ARROW, RIGHT_ARROW, SKIP_TO_LAST]
+PAGE_EMBED_CALLABLE = Callable[[int], Union[discord.Embed, Coroutine[Any, Any, discord.Embed]]]
+
+
+class PaginatorView(discord.ui.View):
+    def __init__(self, get_page, page_count, timeout):
+        super().__init__(timeout=timeout)
+        self._page = 0
+        self._pages = page_count
+        self._get_page = get_page
+
+    @discord.ui.button(emoji="⏪", style=discord.ButtonStyle.gray)
+    async def first(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self._page = 0
+        await self.update_message(interaction)
+
+    @discord.ui.button(emoji="⬅️", style=discord.ButtonStyle.gray)
+    async def back(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self._page -= 1
+        await self.update_message(interaction)
+
+    @discord.ui.button(emoji="➡️", style=discord.ButtonStyle.gray)
+    async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self._page += 1
+        await self.update_message(interaction)
+
+    @discord.ui.button(emoji="⏩", style=discord.ButtonStyle.gray)
+    async def last(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self._page = self._pages - 1
+        await self.update_message(interaction)
+
+    async def update_message(self, interaction: discord.Interaction):
+        # disable buttons if necessary
+        self.children[0].disabled = self._page == 0
+        self.children[1].disabled = self._page == 0
+        self.children[2].disabled = self._page == self._pages - 1
+        self.children[3].disabled = self._page == self._pages - 1
+
+        # update message with new page/embed
+        embed = await self._get_page(self._page)
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    async def on_timeout(self):
+        for child in self.children:
+            child.disabled = True
+
+        await self.message.edit(view=self)
 
 
 class Paginator(commands.Cog):
     def __init__(self, bot: VillagerBotCluster):
         self.bot = bot
-
-    @staticmethod
-    def _create_reaction_check(
-        ctx: Ctx, msg: discord.Message
-    ) -> Callable[[discord.Reaction, discord.User], bool]:
-        def predicate(r: discord.Reaction, u: discord.User):
-            return (str(r.emoji) in NAV_EMOJIS) and ctx.author == u and r.message == msg
-
-        return predicate
 
     @staticmethod
     async def _get_page(get_page: PAGE_EMBED_CALLABLE, page: int) -> discord.Embed:
@@ -49,58 +79,24 @@ class Paginator(commands.Cog):
         get_page: PAGE_EMBED_CALLABLE,
         *,
         timeout: float = 60,
-        page_count: int | None = None,
+        page_count: Optional[int] = None,
     ):
-        page = 0
-        prev_page: int
-
-        # send initial message and add reactions
-        msg = await ctx.reply(embed=(await self._get_page(get_page, page)), mention_author=False)
+        # send initial message
+        embed = await self._get_page(get_page, 0)
 
         if page_count <= 1:
-            return
+            return await ctx.reply(embed=embed, mention_author=False)
 
-        for emoji in NAV_EMOJIS:
-            await msg.add_reaction(emoji)
+        view = PaginatorView(
+            get_page=lambda page: self._get_page(get_page, page),
+            page_count=page_count,
+            timeout=timeout,
+        )
+        view.children[0].disabled = True
+        view.children[1].disabled = True
 
-        while True:
-            prev_page = page
-
-            # wait for a reaction from the user
-            try:
-                reaction: discord.Reaction
-                reaction, _ = await self.bot.wait_for(
-                    "reaction_add", check=self._create_reaction_check(ctx, msg), timeout=timeout
-                )
-            except asyncio.TimeoutError:
-                await asyncio.wait([msg.remove_reaction(e, ctx.me) for e in NAV_EMOJIS])
-                return
-
-            emoji = str(reaction.emoji)
-
-            if emoji == LEFT_ARROW:
-                if page > 0:
-                    page -= 1
-            elif emoji == RIGHT_ARROW:
-                if page < page_count - 1:
-                    page += 1
-            elif emoji == SKIP_TO_LAST:
-                page = page_count - 1
-            elif emoji == BACK_TO_FIRST:
-                page = 0
-
-            else:
-                raise ValueError(emoji)  # shouldn't happen
-
-            # update message with new page/embed
-            if prev_page != page:
-                msg = await msg.edit(embed=(await self._get_page(get_page, page)))
-
-            # remove user's reaction
-            try:
-                await msg.remove_reaction(emoji, ctx.author)
-            except discord.HTTPException:
-                pass
+        message = await ctx.reply(embed=embed, mention_author=False, view=view)
+        view.message = message
 
 
 async def setup(bot: VillagerBotCluster):
