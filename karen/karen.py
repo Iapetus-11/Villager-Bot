120bc303d602c10d5e2fc33a8fb710354dad2723
Infernum11 <81740112+Infernum1@users.noreply.github.com>
2025-01-16 19:33:08 +0530

    fix deprecated behaviour for asyncio.wait()

--- karen/karen.py
+++ karen/karen.py
@@ -275,10 +275,10 @@ ON js.guild_id = ls.guild_id WHERE (COALESCE(js.c, 0) - COALESCE(ls.c, 0)) > 0""
             "RETURNING channel_id, user_id, message_id, reminder",
         )
 
-        broadcast_coros = [self.server.broadcast(PacketType.REMINDER, {**r}) for r in reminders]
+        broadcast_tasks = [asyncio.create_task(self.server.broadcast(PacketType.REMINDER, {**r})) for r in reminders]
 
-        for coros_chunk in chunk_sequence(broadcast_coros, 4):
-            await asyncio.wait(coros_chunk)
+        for tasks_chunk in chunk_sequence(broadcast_tasks, 4):
+            await asyncio.wait(tasks_chunk)
 
     @recurring_task(minutes=30, sleep_first=False)
     async def loop_clear_weekly_leaderboards(self):
