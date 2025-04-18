f48d7d23c293687a9289cb17f608d21daf0d5eac
Milo Weinberg <iapetus011@gmail.com>
2025-04-18 11:38:50 -0400

    Delete karen & common

--- common/models/db/guild.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Guild(BaseModel):
-    guild_id: int
-    prefix: str
-    language: str
-    mc_server: str | None
-    do_replies: bool
--- common/models/db/item.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    sell_price: int
-    amount: int
-    sticky: bool
-    sellable: bool
