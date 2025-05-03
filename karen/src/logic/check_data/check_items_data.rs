7ad3958c466b9de6cca0f3a4f2ccca2021c680b2
Milo Weinberg <iapetus011@gmail.com>
2025-05-03 19:43:09 -0400

    Start on basic karen cli

--- karen/src/logic/check_data/check_items_data.rs
+++ karen/src/logic/check_data/check_items_data.rs
@@ -1,3 +1,13 @@
-pub fn check_items_data(data: &ItemsData) -> Result<(), ItemsDataError> {
+use thiserror::Error;
 
+use crate::models::data::items::ItemsData;
+
+#[derive(Debug, Error)]
+pub enum ItemsDataError {
+
+}
+
+
+pub fn check_items_data(data: &ItemsData) -> Result<(), ItemsDataError> {
+    todo!()
 }
--- karen/src/main.rs
+++ karen/src/main.rs
@@ -1,11 +1,13 @@
-use std::{error::Error as StdError, sync::Arc};
+use std::{env, error::Error as StdError, sync::Arc};
 
-use models::data;
+use config::Config;
+use models::data::{self, items::ItemsData};
 use poem::{
     EndpointExt, Server,
     listener::TcpListener,
     middleware::{AddData, CatchPanic, NormalizePath, TrailingSlash},
 };
+use thiserror::Error;
 
 mod common;
 mod config;
@@ -14,17 +16,16 @@ mod models;
 mod routes;
 mod utils;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn StdError>> {
-    let config = Arc::new(config::load());
+#[derive(Debug, Error)]
+pub enum CliError {
+    #[error("{0:#?} is not a valid command, must be one of: api")]
+    UnknownCommand(String),
 
-    let items_data = Arc::new(data::items::load()?);
+    #[error("You must specify a command that is one of: api")]
+    MissingCommand,
+}
 
-    let db_pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(config.database_pool_size)
-        .connect(&config.database_url)
-        .await?;
-
+async fn run_api(config: Arc<Config>, items_data: Arc<ItemsData>, db_pool: sqlx::Pool<sqlx::Postgres>) -> Result<(), Box<dyn StdError>> {
     let app = routes::setup_routes()
         .with(NormalizePath::new(TrailingSlash::Always))
         .with(AddData::new(config.clone()))
@@ -42,4 +43,30 @@ async fn main() -> Result<(), Box<dyn StdError>> {
         .await?;
 
     Ok(())
+}
+
+fn run_data_validation() -> Result<(), Box<dyn StdError>> {
+    todo!();
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn StdError>> {
+    let mut args =  env::args().skip(1);
+    let command = args.next();
+    let _args = args.collect::<Vec<_>>();
+
+    let config = Arc::new(config::load());
+
+    let items_data = Arc::new(data::items::load()?);
+
+    let db_pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(config.database_pool_size)
+        .connect(&config.database_url)
+        .await?;
+
+    match command.unwrap_or("".into()).as_str() {
+        "api" => run_api(config, items_data, db_pool).await,
+        "" => Err(CliError::MissingCommand.into()),
+        unknown_command => Err(CliError::UnknownCommand(unknown_command.to_string()).into()),
+    }
 }
