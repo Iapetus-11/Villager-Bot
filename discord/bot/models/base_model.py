f9b68570b5204060d155211a25f43ab5500e64c3
Milo Weinberg <iapetus011@gmail.com>
2025-05-10 02:11:45 -0400

    Improve base pydantic models

--- discord/bot/models/base_model.py
+++ discord/bot/models/base_model.py
@@ -1,17 +1,15 @@
 from functools import cached_property
 
 from pydantic import BaseModel as PydanticBaseModel
+from pydantic import ConfigDict
 
 
 class BaseModel(PydanticBaseModel):
-    class Config:
-        validate_default = True
-        ignored_types = (cached_property,)
+    model_config: ConfigDict = ConfigDict(validate_default=True, ignored_types=(cached_property,))
 
     def __getitem__(self, key: str):
         return getattr(self, key)
 
 
 class ImmutableBaseModel(BaseModel):
-    class Config:
-        frozen = True
+    model_config: ConfigDict = {**BaseModel.model_config, **ConfigDict(frozen=True)}
