5b8501b3fc30d701c3a69d799da0f914327c3f2a
Milo Weinberg <iapetus011@gmail.com>
2025-05-10 23:29:51 -0400

    Add missing typehints

--- discord/bot/logic/formatting.py
+++ discord/bot/logic/formatting.py
@@ -1,4 +1,7 @@
-def format_required_items(d: object, shop_item: object, amount: int = 1):
+from bot.models.data import Data, ShopItem
+
+
+def format_required_items(d: Data, shop_item: ShopItem, amount: int = 1):
     base = f" {shop_item.buy_price * amount}{d.emojis.emerald}"
 
     for req_item, req_amount in shop_item.requires.get("items", {}).items():
