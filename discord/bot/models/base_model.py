3b68758fea66c6b160880973e985094370d75f47
Milo Weinberg <iapetus011@gmail.com>
2025-05-16 07:20:22 -0400

    Fix BaseModel typing

--- discord/bot/models/base_model.py
+++ discord/bot/models/base_model.py
@@ -1,15 +1,16 @@
 from functools import cached_property
+from typing import ClassVar
 
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import ConfigDict
 
 
 class BaseModel(PydanticBaseModel):
-    model_config: ConfigDict = ConfigDict(validate_default=True, ignored_types=(cached_property,))
+    model_config: ClassVar[ConfigDict] = ConfigDict(validate_default=True, ignored_types=(cached_property,))
 
     def __getitem__(self, key: str):
         return getattr(self, key)
 
 
 class ImmutableBaseModel(BaseModel):
-    model_config: ConfigDict = {**BaseModel.model_config, **ConfigDict(frozen=True)}
+    model_config: ClassVar[ConfigDict] = {**BaseModel.model_config, **ConfigDict(frozen=True)}
