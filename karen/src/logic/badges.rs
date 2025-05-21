dd82d5b39bc8ab3eb501ef12bd93b34baeb47694
Milo Weinberg <iapetus011@gmail.com>
2025-05-21 07:36:52 -0400

    Remove old comment

--- karen/src/logic/badges.rs
+++ karen/src/logic/badges.rs
@@ -22,8 +22,6 @@ pub async fn get_user_badges(
     ).fetch_optional(&mut *db).await
 }
 
-// Port https://github.com/Iapetus-11/Villager-Bot/blob/8a36c4e565f4bf5ebcbd19689a3c7357c5f3805b/bot/cogs/core/badges.py#L18
-
 fn get_badge_keys(badges: &Badges) -> Vec<String> {
     let mut badge_keys = Vec::<String>::new();
 
