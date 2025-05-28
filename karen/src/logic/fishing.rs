1af937e2f2746e792b7da6526d1ed88c06e67b52
Milo Weinberg <iapetus011@gmail.com>
2025-05-27 20:08:46 -0400

    Start on fish rework

--- karen/src/logic/fishing.rs
+++ karen/src/logic/fishing.rs
@@ -1,6 +1,19 @@
+use std::collections::HashMap;
+
+use chrono::{Datelike, Timelike, Utc};
 use sqlx::PgConnection;
 
 use crate::common::data::FISHING_DATA;
+
+pub fn get_current_fish_prices() -> HashMap<String, i64> {
+    let duration_since_start_of_year = Utc::now() - Utc::now().with_month(1).unwrap().with_day(1).unwrap().with_minute(0).unwrap();
+
+    let elapsed_weeks = duration_since_start_of_year.num_weeks();
+    let elapsed_days = duration_since_start_of_year.num_days();
+    let elapsed_hours = duration_since_start_of_year.num_hours();
+
+    
+}
 
 pub async fn randomize_fish_prices(db: &mut PgConnection) -> Result<(), sqlx::Error> {
     for fish in FISHING_DATA.fish.values() {
